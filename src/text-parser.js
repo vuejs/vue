5541b971ed4e0ce793a034057025bb7be117a12b
Evan You <yyx990803@gmail.com>
2013-08-08 17:08:49 -0400

    text parser

--- src/text-parser.js
+++ src/text-parser.js
@@ -11,13 +11,31 @@ function escapeRegex (val) {
 
 module.exports = {
 
+    /*
+     *  Parse a piece of text, return an array of tokens
+     */
     parse: function (node) {
-        
+        var text = node.nodeValue
+        if (!BINDING_RE.test(text)) return null
+        var m, i, tokens = []
+        do {
+            m = text.match(BINDING_RE)
+            if (!m) break
+            i = m.index
+            if (i > 0) tokens.push(text.slice(0, i))
+            tokens.push({ key: m[1] })
+            text = text.slice(i + m[0].length)
+        } while (true)
+        if (text.length) tokens.push(text)
+        return tokens
     },
 
+    /*
+     *  Build interpolate tag regex from config settings
+     */
     buildRegex: function () {
         var open = escapeRegex(config.interpolateTags.open),
             close = escapeRegex(config.interpolateTags.close)
-        BINDING_RE = new RegExp(open + '(.*?)' + close)
+        BINDING_RE = new RegExp(open + '(.+?)' + close)
     }
 }
