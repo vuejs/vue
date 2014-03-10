9a756121088483c9d9ea258d647d7e3dd9ce1ce3
Evan You <yyou@google.com>
2014-03-10 13:07:16 -0400

    slightly improve text parser perf (10%)

--- src/text-parser.js
+++ src/text-parser.js
@@ -1,18 +1,20 @@
-var BINDING_RE = /{{{?(.+?)}?}}/,
-    TRIPLE_RE = /{{{.+}}}/
+var BINDING_RE = /{{{?(.+?)}?}}/
 
 /**
  *  Parse a piece of text, return an array of tokens
  */
 function parse (text) {
     if (!BINDING_RE.test(text)) return null
-    var m, i, token, tokens = []
+    var m, i, token, match, tokens = []
     /* jshint boss: true */
     while (m = text.match(BINDING_RE)) {
         i = m.index
         if (i > 0) tokens.push(text.slice(0, i))
         token = { key: m[1].trim() }
-        token.html = TRIPLE_RE.test(m[0])
+        match = m[0]
+        token.html =
+            match.charAt(2) === '{' &&
+            match.charAt(match.length - 3) === '}'
         tokens.push(token)
         text = text.slice(i + m[0].length)
     }
