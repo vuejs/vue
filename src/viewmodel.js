b784efe7488345088bfc0653711ba711deb9fc00
Ted Patrick <ted@tedpatrick.com>
2014-07-08 14:21:11 -0700

    Update viewmodel.js
    Allow passing false to constructor to bypass compilation. Add vue.init( config) to defer setting configuration after construction has completed.

--- src/viewmodel.js
+++ src/viewmodel.js
@@ -16,13 +16,22 @@ var Compiler   = require('./compiler'),
  *  and a few reserved methods
  */
 function ViewModel (options) {
-    // just compile. options are passed directly to compiler
+    // compile if options passed, if false return. options are passed directly to compiler
+    if( options === false ) return;
     new Compiler(this, options)
 }
 
 // All VM prototype methods are inenumerable
 // so it can be stringified/looped through as raw data
 var VMProto = ViewModel.prototype
+
+/**
+ *  init allows config compilation after instantiation
+ *  var a = new Vue(false); a.init( config );
+ */
+def(VMProto, 'init', function (options) {
+    new Compiler( this, options )
+})
 
 /**
  *  Convenience function to get a value from
