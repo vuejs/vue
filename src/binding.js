db3a657a672770d09de12ae19356603b6e273952
Evan You <yyx990803@gmail.com>
2014-07-14 21:30:56 -0400

    work on bindings

--- src/binding.js
+++ src/binding.js
@@ -1,5 +1,11 @@
 function Binding () {
-    
+    this.children = Object.create(null)
+}
+
+var p = Binding.prototype
+
+p.addChild = function (key, child) {
+  
 }
 
 module.exports = Binding
--- src/instance/bindings.js
+++ src/instance/bindings.js
@@ -1,7 +1,25 @@
+var Binding = require('../binding')
+
 /**
  * Setup the bindings graph.
  */
 
 exports._initBindings = function () {
+  var root = this._rootBinding = new Binding()
+  // the $data binding points to the root itself!
+  root.addChild('$data', root)
+  // point $parent and $root bindings to their
+  // repective owners.
+  if (this.$parent) {
+    root.addChild('$parent', this.$parent._rootBinding)
+    root.addChild('$root', this.$root._rootBinding)
+  }
+}
+
+/**
+ * Create a binding
+ */
+
+exports._createBinding = function (key) {
   
 }
