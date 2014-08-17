590d5b2207ed2961e95cb5e9db545cf316137abf
Evan You <yyx990803@gmail.com>
2014-08-17 15:36:13 -0400

    new v-if

--- src/directives/if.js
+++ src/directives/if.js
@@ -1,17 +1,55 @@
 var _ = require('../util')
+var templateParser = require('../parse/template')
 
 module.exports = {
 
   bind: function () {
-    
+    var el = this.el
+    if (!el.__vue__) {
+      this.ref = document.createComment('v-if')
+      _.after(this.ref, el)
+      _.remove(el)
+      this.inserted = false
+      if (el.tagName === 'TEMPLATE') {
+        this.el = templateParser.parse(el)
+      }
+    } else {
+      _.warn(
+        'v-if ' + this.expression + ' cannot be ' +
+        'used on an already mounted instance.'
+      )
+    }
   },
 
-  update: function () {
-    
+  update: function (value) {
+    if (value) {
+      if (!this.inserted) {
+        if (!this.childVM) {
+          this.build()
+        }
+        this.childVM.$before(this.ref)
+        this.inserted = true
+      }
+    } else {
+      if (this.inserted) {
+        this.childVM.$remove()
+        this.inserted = false
+      }
+    }
+  },
+
+  build: function () {
+    this.childVM = new _.Vue({
+      el: this.el,
+      parent: this.vm,
+      anonymous: true
+    })
   },
 
   unbind: function () {
-    
+    if (this.childVM) {
+      this.childVM.$destroy()
+    }
   }
 
 }
