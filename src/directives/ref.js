813846a452c42cf49c57aed727230cdc83beea74
Evan You <yyx990803@gmail.com>
2014-09-11 11:07:11 -0400

    ref can only be used on root nodes

--- src/directives/ref.js
+++ src/directives/ref.js
@@ -1,8 +1,16 @@
+var _ = require('../util')
+
 module.exports = {
 
   isLiteral: true,
 
   bind: function () {
+    if (this.el !== this.vm.$el) {
+      _.warn(
+        'v-ref should only be used on instance root nodes.'
+      )
+      return
+    }
     var id = this.expression
     if (id) {
       var owner = this.vm.$parent
