85b64d0bac4d1c81a3259106d48259c46f871b9b
Evan You <yyx990803@gmail.com>
2014-09-17 01:52:48 -0400

    wip tests for v-on

---
+++ test/unit/specs/directives/on_spec.js
@@ -0,0 +1,87 @@
+var _ = require('../../../../src/util')
+var Vue = require('../../../../src/vue')
+
+function trigger (target, event, process) {
+  var e = document.createEvent('HTMLEvents')
+  e.initEvent(event, true, true)
+  if (process) process(e)
+  target.dispatchEvent(e)
+}
+
+if (_.inBrowser) {
+  describe('v-on', function () {
+
+    var el
+    beforeEach(function () {
+      el = document.createElement('div')
+      spyOn(_, 'warn')
+    })
+
+    it('methods', function () {
+      var spy = jasmine.createSpy()
+      var vm = new Vue({
+        el: el,
+        template: '<a v-on="click:test"></a>',
+        data: {a:1},
+        methods: {
+          test: spy
+        }
+      })
+      var a = el.firstChild
+      trigger(a, 'click')
+      expect(spy.calls.count()).toBe(1)
+      vm.$destroy()
+      trigger(a, 'click')
+      expect(spy.calls.count()).toBe(1)
+    })
+
+    it('inline expression', function (done) {
+      var vm = new Vue({
+        el: el,
+        template: '<a v-on="click:a++">{{a}}</a>',
+        data: {a:1}
+      })
+      var a = el.firstChild
+      trigger(a, 'click')
+      _.nextTick(function () {
+        expect(a.textContent).toBe('2')
+        done()
+      })
+    })
+
+    it('with key filter', function (done) {
+      var vm = new Vue({
+        el: el,
+        template: '<a v-on="keyup:test | key enter">{{a}}</a>',
+        data: {a:1},
+        methods: {
+          test: function () {
+            this.a++
+          }
+        }
+      })
+      var a = el.firstChild
+      trigger(a, 'keyup', function (e) {
+        e.keyCode = 13
+      })
+      _.nextTick(function () {
+        expect(a.textContent).toBe('2')
+        done()
+      })
+    })
+
+    it('warn non-function values', function () {
+      var vm = new Vue({
+        el: el,
+        data: { test: 123 },
+        template: '<a v-on="keyup:test"></a>'
+      })
+      expect(_.warn).toHaveBeenCalled()
+    })
+
+    it('iframe', function () {
+      // TODO
+    })
+
+  })
+}
