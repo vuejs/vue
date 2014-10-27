ed72137138e41aa3172aa0cbd2081a025e348800
Evan You <yyx990803@gmail.com>
2014-10-27 14:42:19 -0400

    trigger binding notify in the same order they are added

--- src/binding.js
+++ src/binding.js
@@ -42,8 +42,7 @@ p.removeSub = function (sub) {
  */
 
 p.notify = function () {
-  var i = this.subs.length
-  while (i--) {
+  for (var i = 0, l = this.subs.length; i < l; i++) {
     this.subs[i].update()
   }
 }
