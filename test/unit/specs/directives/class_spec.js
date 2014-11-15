7b0bd12c536c9861a72c9ccf442a66e4ea6fbc0f
Craig Michael Thompson <craig@craigsworks.com>
2014-11-15 22:15:07 +0000

    Add multiple class test to class directive spec

--- test/unit/specs/directives/class_spec.js
+++ test/unit/specs/directives/class_spec.js
@@ -9,7 +9,7 @@ if (_.inBrowser) {
       el = document.createElement('div')
     })
 
-    it('with className', function () {
+    it('single with className', function () {
       el.className = 'haha'
       var dir = {
         el: el,
@@ -22,7 +22,7 @@ if (_.inBrowser) {
       expect(el.className).toBe('haha')
     })
 
-    it('without className', function () {
+    it('single without className', function () {
       el.className = 'haha'
       var dir = {
         el: el,
@@ -36,5 +36,31 @@ if (_.inBrowser) {
       expect(el.className).toBe('haha')
     })
 
+    it('multiple with className', function () {
+      el.className = 'haha'
+      var dir = {
+        el: el,
+        arg: 'test test2',
+        update: def
+      }
+      dir.update(true)
+      expect(el.className).toBe('haha test test2')
+      dir.update(false)
+      expect(el.className).toBe('haha')
+    })
+
+    it('multiple without className', function () {
+      el.className = 'haha'
+      var dir = {
+        el: el,
+        update: def
+      }
+      dir.update('test test2')
+      expect(el.className).toBe('haha test test2')
+      dir.update('what what2')
+      expect(el.className).toBe('haha what what2')
+      dir.update()
+      expect(el.className).toBe('haha')
+    })
   })
 }
