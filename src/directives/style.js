923aee6de8d51e195e1db467481fba564fb7d614
ksa <ksa@issuu.com>
2014-12-01 00:44:31 +0100

    Added v-style support for objects
    Added v-style support for objects

--- src/directives/style.js
+++ src/directives/style.js
@@ -3,43 +3,55 @@ var importantRE = /!important;?$/
 
 module.exports = {
 
+  deep: true,
+
   bind: function () {
     var prop = this.arg
     if (!prop) return
-    if (prop.charAt(0) === '$') {
-      // properties that start with $ will be auto-prefixed
-      prop = prop.slice(1)
-      this.prefixed = true
-    }
     this.prop = prop
   },
 
   update: function (value) {
-    var prop = this.prop
+    if (this.prop) {
+      this.setCssProperty(this.prop, value)
+    } else {
+      if (typeof value === 'object') {
+        for (var prop in value) {
+          this.setCssProperty(prop, value[prop])
+        }
+      } else {
+        this.el.style.cssText = value
+      }
+    }
+  },
+
+  setCssProperty: function (prop, value) {
+    var prefixed = false
+    if (prop.charAt(0) === '$') {
+      // properties that start with $ will be auto-prefixed
+      prop = prop.slice(1)
+      prefixed = true
+    }
     // cast possible numbers/booleans into strings
     if (value != null) {
       value += ''
     }
-    if (prop) {
-      var isImportant = importantRE.test(value)
-        ? 'important'
-        : ''
-      if (isImportant) {
-        value = value.replace(importantRE, '').trim()
+    var isImportant = importantRE.test(value)
+      ? 'important'
+      : ''
+    if (isImportant) {
+      value = value.replace(importantRE, '').trim()
+    }
+    this.el.style.setProperty(prop, value, isImportant)
+    if (prefixed) {
+      var i = prefixes.length
+      while (i--) {
+        this.el.style.setProperty(
+          prefixes[i] + prop,
+          value,
+          isImportant
+        )
       }
-      this.el.style.setProperty(prop, value, isImportant)
-      if (this.prefixed) {
-        var i = prefixes.length
-        while (i--) {
-          this.el.style.setProperty(
-            prefixes[i] + prop,
-            value,
-            isImportant
-          )
-        }
-      }
-    } else {
-      this.el.style.cssText = value
     }
   }
 
