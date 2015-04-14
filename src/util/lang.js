8eed69c6de9041a27f9370adf25d8a7bd3b380ce
Nathan Jovin <njovin@zingleme.com>
2015-04-13 22:24:07 -0700

    #772 cast object keys to strings rather than handling numeric input specifically

--- src/util/lang.js
+++ src/util/lang.js
@@ -6,10 +6,7 @@
  */
 
 exports.isReserved = function (str) {
-  if(typeof str == 'number') {
-    return false;
-  }
-  var c = str.charCodeAt(0)
+  var c = (str + '').charCodeAt(0)
   return c === 0x24 || c === 0x5F
 }
 
