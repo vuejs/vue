d869ba0550956c3ceda8fdc0162dfbe2ce2447a4
Evan You <yyx990803@gmail.com>
2015-05-12 13:59:06 -0400

    list items for async component tests

---
+++ test/unit/specs/async_component_spec.js
@@ -0,0 +1,17 @@
+describe('Async components', function () {
+
+  describe('v-component', function () {
+    // - normal
+    // - dynamic
+    // - nested component caching
+    // - invalidate pending callback on teardown
+    // - avoid duplicate requests
+  })
+
+  describe('v-repeat', function () {
+    // - normal
+    // - invalidate on teardown
+    // - warn for dynamic
+  })
+
+})
