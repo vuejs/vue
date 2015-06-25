a901c06ed8dfd438e4490d7a275eec476590181b
Evan You <yyx990803@gmail.com>
2015-06-25 14:42:06 -0400

    check common tag for element directives

--- src/compiler/compile.js
+++ src/compiler/compile.js
@@ -561,6 +561,7 @@ function makePropsLinkFn (props) {
 
 function checkElementDirectives (el, options) {
   var tag = el.tagName.toLowerCase()
+  if (_.commonTagRE.test(tag)) return
   var def = resolveAsset(options, 'elementDirectives', tag)
   if (def) {
     return makeTerminalNodeLinkFn(el, tag, '', options, def)
