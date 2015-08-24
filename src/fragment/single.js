546cff09a9858478671d0bc1c23b22c5a1385448
Evan You <yyx990803@gmail.com>
2015-08-23 22:19:56 -0400

    transition for v-for

--- src/fragment/single.js
+++ src/fragment/single.js
@@ -1,4 +1,5 @@
 var _ = require('../util')
+var transition = require('../transition')
 
 /**
  * Single-node fragment, optimize insertion/removal for
@@ -17,16 +18,22 @@ function SingleFragment (node, unlink, scope, id) {
   this.id = id
   this.scope = scope
   this.unlink = unlink
+  this.inserted = false
 }
 
 /**
  * Insert fragment before target.
  *
  * @param {Node} target
+ * @param {Boolean} trans
  */
 
-SingleFragment.prototype.before = function (target) {
-  _.before(this.node, target)
+SingleFragment.prototype.before = function (target, trans) {
+  var method = trans !== false
+    ? transition.before
+    : _.before
+  method(this.node, target, this.scope)
+  this.inserted = true
 }
 
 /**
@@ -34,7 +41,8 @@ SingleFragment.prototype.before = function (target) {
  */
 
 SingleFragment.prototype.remove = function () {
-  _.remove(this.node)
+  transition.remove(this.node, this.scope)
+  this.inserted = false
 }
 
 /**
