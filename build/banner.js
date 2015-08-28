b36cd5502b767666a1e35f9483b1e7998f8875ba
Evan You <yyx990803@gmail.com>
2015-08-28 13:04:13 -0400

    add version to Vue

--- build/banner.js
+++
@@ -1,8 +0,0 @@
-var version =
-  process.env.VUE_VERSION ||
-  require('../package.json').version
-
-module.exports =
-  'Vue.js v' + version + '\n' +
-  '(c) ' + new Date().getFullYear() + ' Evan You\n' +
-  'Released under the MIT License.'
--- build/grunt-tasks/build.js
+++ build/grunt-tasks/build.js
@@ -12,6 +12,27 @@ module.exports = function (grunt) {
     var devConfig = require('../webpack.build.dev.config')
     var prodConfig = require('../webpack.build.prod.config')
 
+    // handle version
+    var version =
+      process.env.VUE_VERSION ||
+      require('../../package.json').version
+
+    // add banner
+    var banner = new webpack.BannerPlugin(
+      'Vue.js v' + version + '\n' +
+      '(c) ' + new Date().getFullYear() + ' Evan You\n' +
+      'Released under the MIT License.'
+    )
+    devConfig.plugins.unshift(banner)
+    prodConfig.plugins.unshift(banner)
+
+    // update main file
+    var main = fs
+      .readFileSync('src/vue.js', 'utf-8')
+      .replace(/Vue\.version = '[\d\.]+'/, "Vue.version = '" + version + "'")
+    fs.writeFileSync('src/vue.js', main)
+
+    // build
     webpack(devConfig, function (err, stats) {
       if (err) return done(err)
       report('dist/vue.js')
