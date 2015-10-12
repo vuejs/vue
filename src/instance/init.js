4498289fb1532a916ab03df444214d7cbf1b3f0c
Evan You <yyx990803@gmail.com>
2015-10-12 12:00:14 -0400

    lifecycle: add init hook

--- src/instance/init.js
+++ src/instance/init.js
@@ -95,6 +95,9 @@ exports._init = function (options) {
   // it will be filled up in _initScope().
   this._data = {}
 
+  // call init hook
+  this._callHook('init')
+
   // initialize data observation and scope inheritance.
   this._initState()
 
