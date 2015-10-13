4053b09b00b22318eacc7fec154bc0d649b49408
Evan You <yyx990803@gmail.com>
2015-10-13 12:38:23 -0400

    Make sure we get Jackie Chan's name right

--- examples/grid/grid.js
+++ examples/grid/grid.js
@@ -34,7 +34,7 @@ var demo = new Vue({
     gridData: [
       { name: 'Chuck Norris', power: Infinity },
       { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jacky Chang', power: 7000 },
+      { name: 'Jackie Chan', power: 7000 },
       { name: 'Jet Li', power: 8000 }
     ]
   }
