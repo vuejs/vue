9af9c29907d7ae234ca5d03434da094efed4b76a
Evan You <yyx990803@gmail.com>
2015-10-16 21:12:06 -0400

    wathcer: improve warning check for two-way bindings for v-for with filters (close #1433)

--- src/watcher.js
+++ src/watcher.js
@@ -145,17 +145,26 @@ Watcher.prototype.set = function (value) {
   }
   // two-way sync for v-for alias
   var forContext = scope.$forContext
-  if (forContext && forContext.alias === this.expression) {
-    if (forContext.filters) {
-      process.env.NODE_ENV !== 'production' && _.warn(
+  if (process.env.NODE_ENV !== 'production') {
+    if (
+      forContext &&
+      forContext.filters &&
+      (new RegExp(forContext.alias + '\\b')).test(this.expression)
+    ) {
+      _.warn(
         'It seems you are using two-way binding on ' +
-        'a v-for alias, and the v-for has filters. ' +
-        'This will not work properly. Either remove the ' +
-        'filters or use an array of objects and bind to ' +
-        'object properties instead.'
+        'a v-for alias (' + this.expression + '), and the ' +
+        'v-for has filters. This will not work properly. ' +
+        'Either remove the filters or use an array of ' +
+        'objects and bind to object properties instead.'
       )
-      return
     }
+  }
+  if (
+    forContext &&
+    forContext.alias === this.expression &&
+    !forContext.filters
+  ) {
     if (scope.$key) { // original is an object
       forContext.rawValue[scope.$key] = value
     } else {
