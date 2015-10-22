d7d73db6680b70b0f9409d8e40ee87d9d03a00cc
Evan You <yyx990803@gmail.com>
2015-10-22 14:36:57 -0400

    fix :class binding array syntax cleanup (fix #1458)

--- src/directives/class.js
+++ src/directives/class.js
@@ -57,7 +57,7 @@ module.exports = {
         addClass(this.el, value[i])
       }
     }
-    this.prevKeys = value
+    this.prevKeys = value.slice()
   },
 
   cleanup: function (value) {
@@ -65,7 +65,7 @@ module.exports = {
       var i = this.prevKeys.length
       while (i--) {
         var key = this.prevKeys[i]
-        if (!value || !contains(value, key)) {
+        if (key && (!value || !contains(value, key))) {
           removeClass(this.el, key)
         }
       }
