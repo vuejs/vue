18c8629844fb0da8f8ec172a60014d8f31512ecf
Evan You <yyx990803@gmail.com>
2015-10-27 13:53:47 -0400

    revert 3333598: fix memory leak when replacing arrays

--- src/observer/array.js
+++ src/observer/array.js
@@ -28,7 +28,7 @@ var arrayMethods = Object.create(arrayProto)
     }
     var result = original.apply(this, args)
     var ob = this.__ob__
-    var inserted, removed
+    var inserted
     switch (method) {
       case 'push':
         inserted = args
@@ -38,17 +38,11 @@ var arrayMethods = Object.create(arrayProto)
         break
       case 'splice':
         inserted = args.slice(2)
-        removed = result
-        break
-      case 'pop':
-      case 'shift':
-        removed = [result]
         break
     }
     if (inserted) ob.observeArray(inserted)
-    if (removed) ob.unobserveArray(removed)
     // notify change
-    ob.notify()
+    ob.dep.notify()
     return result
   })
 })
--- src/observer/index.js
+++ src/observer/index.js
@@ -91,42 +91,7 @@ Observer.prototype.walk = function (obj) {
 Observer.prototype.observeArray = function (items) {
   var i = items.length
   while (i--) {
-    var ob = Observer.create(items[i])
-    if (ob) {
-      (ob.parents || (ob.parents = [])).push(this)
-    }
-  }
-}
-
-/**
- * Remove self from the parent list of removed objects.
- *
- * @param {Array} items
- */
-
-Observer.prototype.unobserveArray = function (items) {
-  var i = items.length
-  while (i--) {
-    var ob = items[i] && items[i].__ob__
-    if (ob) {
-      ob.parents.$remove(this)
-    }
-  }
-}
-
-/**
- * Notify self dependency, and also parent Array dependency
- * if any.
- */
-
-Observer.prototype.notify = function () {
-  this.dep.notify()
-  var parents = this.parents
-  if (parents) {
-    var i = parents.length
-    while (i--) {
-      parents[i].notify()
-    }
+    Observer.create(items[i])
   }
 }
 
@@ -216,6 +181,12 @@ function defineReactive (obj, key, val) {
         dep.depend()
         if (childOb) {
           childOb.dep.depend()
+        }
+        if (_.isArray(val)) {
+          for (var e, i = 0, l = val.length; i < l; i++) {
+            e = val[i]
+            e && e.__ob__ && e.__ob__.dep.depend()
+          }
         }
       }
       return val
