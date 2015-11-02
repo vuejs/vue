abc9a238f452d7fddd579de5e928dae74d31bc17
Evan You <yyx990803@gmail.com>
2015-11-02 00:50:48 -0500

    do not warn vue-loader scoped css attributes

--- src/compiler/compile.js
+++ src/compiler/compile.js
@@ -211,17 +211,23 @@ exports.compileRoot = function (el, options, contextOptions) {
     }
   } else if (process.env.NODE_ENV !== 'production' && containerAttrs) {
     // warn container directives for fragment instances
-    var names = containerAttrs.map(function (attr) {
-      return '"' + attr.name + '"'
-    }).join(', ')
-    var plural = containerAttrs.length > 1
-    _.warn(
-      'Attribute' + (plural ? 's ' : ' ') + names +
-      (plural ? ' are' : ' is') + ' ignored on component ' +
-      '<' + options.el.tagName.toLowerCase() + '> because ' +
-      'the component is a fragment instance: ' +
-      'http://vuejs.org/guide/components.html#Fragment_Instance'
-    )
+    var names = containerAttrs
+      .filter(function (attr) {
+        return attr.name.indexOf('_v-') < 0
+      })
+      .map(function (attr) {
+        return '"' + attr.name + '"'
+      })
+    if (names.length) {
+      var plural = names.length > 1
+      _.warn(
+        'Attribute' + (plural ? 's ' : ' ') + names.join(',') +
+        (plural ? ' are' : ' is') + ' ignored on component ' +
+        '<' + options.el.tagName.toLowerCase() + '> because ' +
+        'the component is a fragment instance: ' +
+        'http://vuejs.org/guide/components.html#Fragment_Instance'
+      )
+    }
   }
 
   return function rootLinkFn (vm, el, scope) {
