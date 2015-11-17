d734b61dc6fb5556dcb23e5a4c4a17770f588d93
Evan You <yyx990803@gmail.com>
2015-11-16 21:36:02 -0500

    tweak build

--- build/ci.sh
+++ build/ci.sh
@@ -2,7 +2,7 @@ if [ -z "$CI_PULL_REQUEST" ]
 then
   npm run lint &&\
   npm run cover &&\
-  node build/codecov.js &&\
+  cat ./coverage/lcov.info | ./node_modules/.bin/codecov
   npm run build &&\
   npm run e2e &&\
   npm run sauce-all
