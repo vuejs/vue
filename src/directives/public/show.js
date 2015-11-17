c80b78bc39a9f14d2f580d486e4f72b0f23b88a2
Evan You <yyx990803@gmail.com>
2015-11-17 10:25:50 -0500

    v-show

--- src/directives/public/show.js
+++ src/directives/public/show.js
@@ -1,9 +1,9 @@
 import { attr } from '../../util'
 import { applyTransition } from '../../transition'
 
-module.exports = {
+export default {
 
-  bind: function () {
+  bind () {
     // check else block
     var next = this.el.nextElementSibling
     if (next && attr(next, 'v-else') !== null) {
@@ -11,14 +11,14 @@ module.exports = {
     }
   },
 
-  update: function (value) {
+  update (value) {
     this.apply(this.el, value)
     if (this.elseEl) {
       this.apply(this.elseEl, !value)
     }
   },
 
-  apply: function (el, value) {
+  apply (el, value) {
     applyTransition(el, value ? 1 : -1, function () {
       el.style.display = value ? '' : 'none'
     }, this.vm)
