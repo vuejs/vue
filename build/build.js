fe907ce58ad927bb81556889233de2233dbfc08d
Evan You <yyx990803@gmail.com>
2015-11-17 12:28:10 -0500

    update build script

--- build/build.js
+++ build/build.js
@@ -1,9 +1,11 @@
 var fs = require('fs')
+var zlib = require('zlib')
 var rollup = require('rollup')
 var uglify = require('uglify-js')
 var babel = require('rollup-plugin-babel')
 var replace = require('rollup-plugin-replace')
 var version = process.env.VERSION || require('../package.json').version
+
 var banner =
   '/*!\n' +
   ' * Vue.js v' + version + '\n' +
@@ -11,6 +13,42 @@ var banner =
   ' * Released under the MIT License.\n' +
   ' */'
 
+// CommonJS build.
+// this is used as the "main" field in package.json
+// and used by bundlers like Webpack and Browserify.
+rollup.rollup({
+  entry: 'src/index.js',
+  plugins: [
+    babel({
+      loose: 'all'
+    })
+  ]
+}).then(function (bundle) {
+  write('dist/vue.common.js', bundle.generate({
+    format: 'cjs'
+  }).code)
+}).catch(logError)
+
+// Standalone Dev Build
+rollup.rollup({
+  entry: 'src/index.js',
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': "'development'"
+    }),
+    babel({
+      loose: 'all'
+    })
+  ]
+}).then(function (bundle) {
+  write('dist/vue.js', bundle.generate({
+    format: 'umd',
+    banner: banner,
+    moduleName: 'Vue'
+  }).code)
+}).catch(logError)
+
+// Standalone Production Build
 rollup.rollup({
   entry: 'src/index.js',
   plugins: [
@@ -18,34 +56,46 @@ rollup.rollup({
       'process.env.NODE_ENV': "'production'"
     }),
     babel({
-      loose: 'all',
-      sourceMap: true
+      loose: 'all'
     })
   ]
 }).then(function (bundle) {
-  bundle.write({
-    format: 'cjs',
-    dest: 'dist/vue.common.js'
-  }).then(function () {
-    console.log('built: dist/vue.common.js')
-  })
-  return bundle.write({
+  var code = bundle.generate({
     format: 'umd',
-    banner: banner,
-    moduleName: 'Vue',
-    sourceMap: true,
-    dest: 'dist/vue.js'
+    moduleName: 'Vue'
+  }).code
+  var minified = banner + '\n' + uglify.minify(code, {
+    fromString: true
+  }).code
+  write('dist/vue.min.js', minified, zip)
+}).catch(logError)
+
+function write (dest, code, cb) {
+  fs.writeFile(dest, code, function (err) {
+    if (err) throw err
+    console.log(blue('built: ') + dest + ' ' + getSize(code))
+    cb && cb()
   })
-}).then(function () {
-  console.log('built: ' + 'dist/vue.js')
-  fs.writeFile(
-    'dist/vue.min.js',
-    banner + '\n' + uglify.minify('dist/vue.js').code,
-    function (err) {
+}
+
+function zip () {
+  fs.readFile('dist/vue.min.js', function (err, buf) {
+    if (err) throw err
+    zlib.gzip(buf, function (err, buf) {
       if (err) throw err
-      console.log('built: ' + 'dist/vue.min.js')
-    }
-  )
-}).catch(function (e) {
+      write('dist/vue.min.js.gz', buf)
+    })
+  })
+}
+
+function getSize (code) {
+  return '(' + (code.length / 1024).toFixed(2) + 'kb)'
+}
+
+function logError (e) {
   console.log(e)
-})
+}
+
+function blue (str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+}
