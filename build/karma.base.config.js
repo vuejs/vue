347b0f61069f50266a4ffcea3813168d558dc7f6
Evan You <yyx990803@gmail.com>
2015-11-16 21:14:27 -0500

    update karma test config

---
+++ build/karma.base.config.js
@@ -0,0 +1,26 @@
+// shared config for all unit tests
+module.exports = {
+  frameworks: ['jasmine'],
+  files: [
+    '../test/unit/lib/jquery.js',
+    '../test/unit/specs/index.js'
+  ],
+  preprocessors: {
+    '../test/unit/specs/index.js': ['webpack']
+  },
+  webpack: {
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /test|node_modules/,
+          loader: 'babel?optional[]=runtime&loose=all'
+        }
+      ]
+    }
+  },
+  webpackMiddleware: {
+    noInfo: true
+  },
+  singleRun: true
+}
