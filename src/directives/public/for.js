5fe6e55a10a5efaf7e27ffac5fa8d9f5dece30b6
Evan You <yyx990803@gmail.com>
2015-11-23 10:45:33 -0500

    fix v-ref on v-for when repeated block is a sub-fragment (fix #1850)

--- src/directives/public/for.js
+++ src/directives/public/for.js
@@ -581,7 +581,14 @@ function findPrevFrag (frag, anchor, id) {
  */
 
 function findVmFromFrag (frag) {
-  return frag.node.__vue__ || frag.node.nextSibling.__vue__
+  let node = frag.node
+  // handle multi-node frag
+  if (frag.end) {
+    while (!node.__vue__ && node !== frag.end && node.nextSibling) {
+      node = node.nextSibling
+    }
+  }
+  return node.__vue__
 }
 
 /**
