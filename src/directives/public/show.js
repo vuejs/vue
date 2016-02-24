5221d656c4a6adcae9848a186a65366e635294c5
Bill Criswell <bill.criswell@gmail.com>
2016-02-24 12:46:41 -0500

    visiblity modifier for v-show

--- src/directives/public/show.js
+++ src/directives/public/show.js
@@ -12,20 +12,24 @@ export default {
   },
 
   update (value) {
-    this.apply(this.el, value)
+    this.apply(this.el, value, this.modifiers)
     if (this.elseEl) {
-      this.apply(this.elseEl, !value)
+      this.apply(this.elseEl, !value, this.modifiers)
     }
   },
 
-  apply (el, value) {
+  apply (el, value, modifiers) {
     if (inDoc(el)) {
       applyTransition(el, value ? 1 : -1, toggle, this.vm)
     } else {
       toggle()
     }
     function toggle () {
-      el.style.display = value ? '' : 'none'
+      if (modifiers && modifiers.visibility) {
+        el.style.visibility = value ? 'visible' : 'hidden'
+      } else {
+        el.style.display = value ? '' : 'none'
+      }
     }
   }
 }
--- test/unit/specs/directives/public/show_spec.js
+++ test/unit/specs/directives/public/show_spec.js
@@ -14,7 +14,7 @@ describe('v-show', function () {
     document.body.removeChild(el)
   })
 
-  it('should work', function () {
+  it('should toggle display', function () {
     var dir = {
       el: el,
       update: def.update,
@@ -26,6 +26,29 @@ describe('v-show', function () {
     dir.update(true)
     expect(el.style.display).toBe('')
     expect(transition.applyTransition).toHaveBeenCalled()
+  })
+
+  it('should toggle visibility', function (done) {
+    var vm = new Vue({
+      el: el,
+      template:
+        '<p v-show.visibility="ok">YES</p>' +
+        '<p v-else>NO</p>',
+      data: {
+        ok: true
+      }
+    })
+
+    expect(el.children[0].style.visibility).toBe('visible')
+    expect(el.children[1].style.visibility).toBe('hidden')
+    expect(transition.applyTransition.calls.count()).toBe(2)
+    vm.ok = false
+    Vue.nextTick(function () {
+      expect(el.children[0].style.visibility).toBe('hidden')
+      expect(el.children[1].style.visibility).toBe('visible')
+      expect(transition.applyTransition.calls.count()).toBe(4)
+      done()
+    })
   })
 
   it('should work with v-else', function (done) {
