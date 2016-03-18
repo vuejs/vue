4f077e4c03e6d2ecd7ff50e874d3f2d341b5646a
James Brooks <james@bluebaytravel.co.uk>
2016-03-18 12:47:10 +0000

    Allow currency filter to specify places

--- src/filters/index.js
+++ src/filters/index.js
@@ -63,20 +63,23 @@ export default {
   /**
    * 12345 => $12,345.00
    *
-   * @param {String} sign
+   * @param {String} currency
+   * @param {Number} places
    */
 
-  currency (value, currency) {
+  currency (value, currency, places) {
     value = parseFloat(value)
     if (!isFinite(value) || (!value && value !== 0)) return ''
     currency = currency != null ? currency : '$'
-    var stringified = Math.abs(value).toFixed(2)
-    var _int = stringified.slice(0, -3)
-    var i = _int.length % 3
+    places = places != null ? places : 2
+    var stringified = Math.abs(value).toFixed(places)
+    var dotplace = Math.abs(places + 1)
+    var _int = stringified.slice(0, -dotplace)
+    var i = _int.length % dotplace
     var head = i > 0
-      ? (_int.slice(0, i) + (_int.length > 3 ? ',' : ''))
+      ? (_int.slice(0, i) + (_int.length > dotplace ? ',' : ''))
       : ''
-    var _float = stringified.slice(-3)
+    var _float = stringified.slice(-dotplace)
     var sign = value < 0 ? '-' : ''
     return sign + currency + head +
       _int.slice(i).replace(digitsRE, '$1,') +
