f1bdaa8bf428a0a520d94244643c770b3e5a60b1
Evan You <yyx990803@gmail.com>
2016-04-12 19:02:30 -0400

    properly initialize state

--- src/instance/index.js
+++ src/instance/index.js
@@ -1,26 +1,19 @@
-import { observe } from '../observer/index'
 import Watcher from '../observer/watcher'
 import { h, patch } from '../vdom/index'
-import { nextTick, isReserved, query } from '../util/index'
+import { nextTick, query } from '../util/index'
+import stateMixin from './internal/state'
 
-export default function Component (options) {
+export default function Vue (options) {
   this.$options = options
-  this._data = options.data
+  this._watchers = []
+  this._initState()
   this._el = query(options.el)
   this._el.innerHTML = ''
-  Object.keys(options.data).forEach(key => proxy(this, key))
-  if (options.methods) {
-    Object.keys(options.methods).forEach(key => {
-      this[key] = options.methods[key].bind(this)
-    })
-  }
-  this._ob = observe(options.data)
-  this._watchers = []
   this._watcher = new Watcher(this, options.render, this._update)
   this._update(this._watcher.value)
 }
 
-Component.prototype._update = function (vtree) {
+Vue.prototype._update = function (vtree) {
   if (!this._tree) {
     patch(this._el, vtree)
   } else {
@@ -29,20 +22,7 @@ Component.prototype._update = function (vtree) {
   this._tree = vtree
 }
 
-function proxy (vm, key) {
-  if (!isReserved(key)) {
-    Object.defineProperty(vm, key, {
-      configurable: true,
-      enumerable: true,
-      get: function proxyGetter () {
-        return vm._data[key]
-      },
-      set: function proxySetter (val) {
-        vm._data[key] = val
-      }
-    })
-  }
-}
+Vue.prototype.__h__ = h
+Vue.nextTick = nextTick
 
-Component.prototype.__h__ = h
-Component.nextTick = nextTick
+stateMixin(Vue)
