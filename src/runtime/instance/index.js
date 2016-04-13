91a04ad0387755ad5a1f6f862e1c5d6ae7276874
Evan You <yyx990803@gmail.com>
2016-04-12 21:21:50 -0400

    refactor state

--- src/runtime/instance/index.js
+++ src/runtime/instance/index.js
@@ -1,12 +1,12 @@
 import Watcher from '../observer/watcher'
 import { h, patch } from '../vdom/index'
 import { nextTick, query } from '../util/index'
-import stateMixin from './internal/state'
+import { initState, setData } from './state'
 
 export default function Vue (options) {
   this.$options = options
   this._watchers = []
-  this._initState()
+  initState(this)
   this._el = query(options.el)
   this._el.innerHTML = ''
   this._watcher = new Watcher(this, options.render, this._update)
@@ -22,7 +22,20 @@ Vue.prototype._update = function (vtree) {
   this._tree = vtree
 }
 
+Vue.prototype.$forceUpdate = function () {
+  this._watcher.run()
+}
+
+Object.defineProperty(Vue.prototype, '$data', {
+  get () {
+    return this._data
+  },
+  set (newData) {
+    if (newData !== this._data) {
+      setData(this, newData)
+    }
+  }
+})
+
 Vue.prototype.__h__ = h
 Vue.nextTick = nextTick
-
-stateMixin(Vue)
---
+++ src/runtime/instance/state.js
@@ -0,0 +1,143 @@
+import Watcher from '../observer/watcher'
+import Dep from '../observer/dep'
+import { observe } from '../observer/index'
+
+import {
+  warn,
+  hasOwn,
+  isReserved,
+  isPlainObject,
+  bind
+} from '../util/index'
+
+export function initState (vm) {
+  initData(vm)
+  initComputed(vm)
+  initMethods(vm)
+}
+
+function initData (vm) {
+  var data = vm.$options.data
+  data = vm._data = typeof data === 'function'
+    ? data()
+    : data || {}
+  if (!isPlainObject(data)) {
+    data = {}
+    process.env.NODE_ENV !== 'production' && warn(
+      'data functions should return an object.',
+      vm
+    )
+  }
+  // proxy data on instance
+  var keys = Object.keys(data)
+  var i = keys.length
+  while (i--) {
+    proxy(vm, keys[i])
+  }
+  // observe data
+  observe(data, vm)
+}
+
+function noop () {}
+
+function initComputed (vm) {
+  var computed = vm.$options.computed
+  if (computed) {
+    for (var key in computed) {
+      var userDef = computed[key]
+      var def = {
+        enumerable: true,
+        configurable: true
+      }
+      if (typeof userDef === 'function') {
+        def.get = makeComputedGetter(userDef, vm)
+        def.set = noop
+      } else {
+        def.get = userDef.get
+          ? userDef.cache !== false
+            ? makeComputedGetter(userDef.get, vm)
+            : bind(userDef.get, vm)
+          : noop
+        def.set = userDef.set
+          ? bind(userDef.set, vm)
+          : noop
+      }
+      Object.defineProperty(vm, key, def)
+    }
+  }
+}
+
+function makeComputedGetter (getter, owner) {
+  var watcher = new Watcher(owner, getter, null, {
+    lazy: true
+  })
+  return function computedGetter () {
+    if (watcher.dirty) {
+      watcher.evaluate()
+    }
+    if (Dep.target) {
+      watcher.depend()
+    }
+    return watcher.value
+  }
+}
+
+function initMethods (vm) {
+  var methods = vm.$options.methods
+  if (methods) {
+    for (var key in methods) {
+      vm[key] = bind(methods[key], vm)
+    }
+  }
+}
+
+function proxy (vm, key) {
+  if (!isReserved(key)) {
+    Object.defineProperty(vm, key, {
+      configurable: true,
+      enumerable: true,
+      get: function proxyGetter () {
+        return vm._data[key]
+      },
+      set: function proxySetter (val) {
+        vm._data[key] = val
+      }
+    })
+  }
+}
+
+function unproxy (vm, key) {
+  if (!isReserved(key)) {
+    delete vm[key]
+  }
+}
+
+export function setData (vm, newData) {
+  newData = newData || {}
+  var oldData = vm._data
+  vm._data = newData
+  var keys, key, i
+  // unproxy keys not present in new data
+  keys = Object.keys(oldData)
+  i = keys.length
+  while (i--) {
+    key = keys[i]
+    if (!(key in newData)) {
+      unproxy(vm, key)
+    }
+  }
+  // proxy keys not already proxied,
+  // and trigger change for changed values
+  keys = Object.keys(newData)
+  i = keys.length
+  while (i--) {
+    key = keys[i]
+    if (!hasOwn(vm, key)) {
+      // new property
+      proxy(vm, key)
+    }
+  }
+  oldData.__ob__.removeVm(vm)
+  observe(newData, vm)
+  vm.$forceUpdate()
+}
