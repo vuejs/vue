1af8c6fc11957a236047fde395bf6c831dddc3b6
Evan You <yyx990803@gmail.com>
2016-04-13 14:55:32 -0400

    separate template-parser and html-parser

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,12 +1,12 @@
-import { parse } from './html-parser'
+import { parse } from './template-parser'
 import { generate } from './codegen/index'
 
 const cache1 = Object.create(null)
 const cache2 = Object.create(null)
 
-export function compile (html, preserveWhiteSpace) {
+export function compile (html, preserveWhitespace) {
   html = html.trim()
-  const cache = preserveWhiteSpace ? cache1 : cache2
+  const cache = preserveWhitespace ? cache1 : cache2
   const hit = cache[html]
-  return hit || (cache[html] = generate(parse(html, preserveWhiteSpace)))
+  return hit || (cache[html] = generate(parse(html, preserveWhitespace)))
 }
