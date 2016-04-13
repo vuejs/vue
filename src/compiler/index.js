fa46e8f88ee9316370d593ccbbbfcc0a068fdf4b
Evan You <yyx990803@gmail.com>
2016-04-12 20:15:56 -0400

    fix caching with preserveWhiteSpace

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,10 +1,12 @@
 import { parse } from './html-parser'
 import { generate } from './codegen/index'
 
-const cache = Object.create(null)
+const cache1 = Object.create(null)
+const cache2 = Object.create(null)
 
 export function compile (html, preserveWhiteSpace) {
   html = html.trim()
+  const cache = preserveWhiteSpace ? cache1 : cache2
   const hit = cache[html]
   return hit || (cache[html] = generate(parse(html, preserveWhiteSpace)))
 }
