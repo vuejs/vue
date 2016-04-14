3e39d7647d68169fac97f0f34c01d962200480e3
Evan You <yyx990803@gmail.com>
2016-04-13 20:54:27 -0400

    allow registering custom compile-time directives

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,5 +1,6 @@
 import { parse } from './parser/index'
 import { generate } from './codegen/index'
+import { directives } from './codegen/directives/index'
 
 const cache1 = Object.create(null)
 const cache2 = Object.create(null)
@@ -9,4 +10,8 @@ export function compile (html, preserveWhitespace) {
   const cache = preserveWhitespace ? cache1 : cache2
   const hit = cache[html]
   return hit || (cache[html] = generate(parse(html, preserveWhitespace)))
+}
+
+export function registerDirective (name, fn) {
+  directives[name] = fn
 }
