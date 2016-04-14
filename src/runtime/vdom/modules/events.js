93978f8469f8e48d00931a528c89259ed04fd233
Evan You <yyx990803@gmail.com>
2016-04-13 20:37:13 -0400

    fix multiple events

--- src/runtime/vdom/modules/events.js
+++ src/runtime/vdom/modules/events.js
@@ -1,9 +1,8 @@
 function arrInvoker (arr) {
-  return function () {
-    // Special case when length is two, for performance
-    arr.length === 2
-      ? arr[0](arr[1])
-      : arr[0].apply(undefined, arr.slice(1))
+  return function (ev) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i](ev)
+    }
   }
 }
 
