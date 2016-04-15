53f922ed61222c792c69d4003ca651e98a1514c8
An Phan <an@construct.sg>
2016-04-15 22:42:58 +0800

    Add support for decimal places in currency filter
    Currently the "currency" filter only accepts one argument: the currency
    symbol. The number of decimal places is fixed as 2. This can cause
    troubles with several currencies out there whose number of decimal places
    can be 0 (e.g. Japanese Yen or Vietnamese Dong) or 3 (e.g. Jordanian
    Dinar).
    This commit modifies the filter to accept an extra optional argument:
    decimal places, which defaults to 2. With this, one can write `{{ 1234 |
    currency '¥' 0 }}` to properly display a Yen value. Backward
    compatibility is maintained.

--- src/filters/index.js
+++ src/filters/index.js
@@ -64,19 +64,25 @@ export default {
    * 12345 => $12,345.00
    *
    * @param {String} sign
+   * @param {Number} decimals Decimal places
    */
 
-  currency (value, currency) {
+  currency (value, currency, decimals) {
     value = parseFloat(value)
     if (!isFinite(value) || (!value && value !== 0)) return ''
     currency = currency != null ? currency : '$'
-    var stringified = Math.abs(value).toFixed(2)
-    var _int = stringified.slice(0, -3)
+    decimals = decimals != null ? decimals : 2
+    var stringified = Math.abs(value).toFixed(decimals)
+    var _int = decimals
+      ? stringified.slice(0, -1 - decimals)
+      : stringified
     var i = _int.length % 3
     var head = i > 0
       ? (_int.slice(0, i) + (_int.length > 3 ? ',' : ''))
       : ''
-    var _float = stringified.slice(-3)
+    var _float = decimals
+      ? stringified.slice(-1 - decimals)
+      : ''
     var sign = value < 0 ? '-' : ''
     return sign + currency + head +
       _int.slice(i).replace(digitsRE, '$1,') +
