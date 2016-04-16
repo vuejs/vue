472784226d7ff2347ef972ed9b01e09b38340c9e
Evan You <yyx990803@gmail.com>
2016-04-16 03:08:23 -0400

    fix e2e tree

--- examples/tree/tree.js
+++ examples/tree/tree.js
@@ -2,7 +2,29 @@
 var data = {
   name: 'My Tree',
   children: [
-    { name: 'hello' }
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        }
+      ]
+    }
   ]
 }
 
