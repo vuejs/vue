7926f6ae83a967edca1a3c812602327f4ff32a18
Evan You <yyx990803@gmail.com>
2016-04-19 13:04:43 -0400

    guard on

--- src/runtime/dom-backend/modules/events.js
+++ src/runtime/dom-backend/modules/events.js
@@ -4,7 +4,7 @@ function updateDOMListeners (oldVnode, vnode) {
   if (!oldVnode.data.on && !vnode.data.on) {
     return
   }
-  const on = vnode.data.on
+  const on = vnode.data.on || {}
   const oldOn = oldVnode.data.on || {}
   updateListeners(on, oldOn, (event, handler, capture) => {
     vnode.elm.addEventListener(event, handler, capture)
