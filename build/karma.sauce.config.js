c07b22010471a458b9ec9dd74eddd5c69094f55d
Evan You <yyx990803@gmail.com>
2016-04-21 11:21:59 -0400

    allow local sauce runs to have the same job id

--- build/karma.sauce.config.js
+++ build/karma.sauce.config.js
@@ -73,7 +73,7 @@ module.exports = function (config) {
     sauceLabs: {
       testName: 'Vue.js unit tests',
       recordScreenshots: false,
-      build: process.env.CIRCLE_BUILD_NUM || Date.now()
+      build: process.env.CIRCLE_BUILD_NUM || process.env.SAUCE_BUILD_ID || Date.now()
     },
     // mobile emulators are really slow
     captureTimeout: 300000,
