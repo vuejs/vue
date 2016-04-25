95a0f3c56c74cca6ff7ea3a992848b163acd803c
Evan You <yyx990803@gmail.com>
2016-04-24 22:52:58 -0400

    naive ssr implementation

--- src/platforms/web/server/create-renderer.js
+++ src/platforms/web/server/create-renderer.js
@@ -1,23 +1,72 @@
-import attrs from './modules/attrs'
-import klass from './modules/class'
-import style from './modules/style'
+import renderAttrs from './modules/attrs'
+import renderClass from './modules/class'
+import renderStyle from './modules/style'
 import show from './directives/show'
-import { renderToString } from './render-to-string'
-import { renderToStream } from './render-to-stream'
+import { isUnaryTag } from 'web/util/index'
 
 export function createComponentRenderer (options = {}) {
   const modules = Object.assign({
-    attrs,
-    style,
-    class: klass
+    attrs: renderAttrs,
+    style: renderStyle,
+    class: renderClass
   }, options.modules)
 
   const directives = Object.assign({
     show
   }, options.directives)
 
+  function renderComponent (component) {
+    component.$mount()
+    return renderElement(component._vnode)
+  }
+
+  function renderNode (node) {
+    return node.child
+      ? renderComponent(node)
+      : node.tag
+        ? renderElement(node)
+        : node.text
+  }
+
+  function renderElement (el) {
+    const startTag = renderStartingTag(el)
+    if (isUnaryTag(el.tag)) {
+      return startTag
+    } else {
+      const children = el.children.map(renderNode).join('')
+      return startTag + children + `</${el.tag}>`
+    }
+  }
+
+  function renderStartingTag (node) {
+    let markup = `<${node.tag}`
+    if (node.data) {
+      // check directives
+      const dirs = node.data.directives
+      if (dirs) {
+        for (let key in dirs) {
+          let dir = directives[key]
+          if (dir) {
+            // directives mutate the node's data
+            // which then gets rendered by modules
+            dir(node)
+          }
+        }
+      }
+      // apply other modules
+      for (let key in node.data) {
+        let renderer = modules[key]
+        if (renderer) {
+          markup += ' ' + renderer(node.data[key])
+        }
+      }
+    }
+    return markup + '>'
+  }
+
   return {
-    renderToString: component => renderToString(component, modules, directives),
-    renderToStream: component => renderToStream(component, modules, directives)
+    renderToString (component) {
+      return renderComponent(component)
+    }
   }
 }
--- src/platforms/web/server/render-to-string.js
+++
@@ -1,3 +0,0 @@
-export function renderToString (component, modules, directives) {
-  component.$mount()
-}
