ba72ac1e8e670fed40275e7b11ad6620cfa27c98
tejitak <tejitak@gmail.com>
2016-05-04 23:16:29 +0900

    add img tag in reserved tag list

--- src/platforms/web/util/element.js
+++ src/platforms/web/util/element.js
@@ -9,7 +9,7 @@ export const namespaceMap = {
 export const isReservedTag = makeMap(
   'html,base,head,link,meta,style,title,' +
   'address,article,footer,header,h1,h2,h3,h4,h5,h6,hgroup,nav,section,' +
-  'div,dd,dl,dt,figcaption,figure,hr,li,main,ol,p,pre,ul,' +
+  'div,dd,dl,dt,figcaption,figure,hr,img,li,main,ol,p,pre,ul,' +
   'a,b,abbr,bdi,bdo,br,cite,code,data,dfn,em,i,kbd,mark,q,rp,rt,rtc,ruby,' +
   's,samp,small,span,strong,sub,sup,time,u,var,wbr,area,audio,map,track,video,' +
   'embed,object,param,source,canvas,script,noscript,del,ins,' +
