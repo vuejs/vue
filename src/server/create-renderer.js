006235785923658e299765436a73d0b64321bab0
Blake Newman <blake.newman@sainsburys.co.uk>
2016-05-10 17:16:32 +0100

    SSR warn for no VUE_ENV and add tests
    - Added test coverage for data observation and VUE environment variable
    - Moved Test helpers into own folder for reusability

--- src/server/create-renderer.js
+++ src/server/create-renderer.js
@@ -1,11 +1,19 @@
 import RenderStream from './render-stream'
 import { createRenderFunction } from './render'
+import { warn } from 'core/util/debug'
 
 export function createRenderer ({
   modules = [],
   directives = {},
   isUnaryTag = (() => false)
 } = {}) {
+  if (process.env.VUE_ENV !== 'server') {
+    warn(
+      'You are using createRenderer without setting VUE_ENV enviroment variable to "server". ' +
+      'It is recommended to set VUE_ENV=server this will help rendering performance, ' +
+      'by turning data observation off.'
+    )
+  }
   const render = createRenderFunction(modules, directives, isUnaryTag)
   return {
     renderToString (component) {
