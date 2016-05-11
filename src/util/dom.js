b913aab7a5a826b57ae64c60998ea0f65c67a0ea
Evan You <yyx990803@gmail.com>
2016-05-11 17:47:00 -0400

    check node presence in inDoc

--- src/util/dom.js
+++ src/util/dom.js
@@ -37,8 +37,9 @@ export function query (el) {
  */
 
 export function inDoc (node) {
+  if (!node) return false
   var doc = node.ownerDocument.documentElement
-  var parent = node && node.parentNode
+  var parent = node.parentNode
   return doc === node ||
     doc === parent ||
     !!(parent && parent.nodeType === 1 && (doc.contains(parent)))
