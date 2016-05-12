35440d9856f40274f573e90619d8e75a9522798a
Blake Newman <blakenoodles@gmail.com>
2016-05-12 06:21:53 +0100

    SSR warn for no VUE_ENV and add tests (#2844)
    - Added test coverage for data observation and VUE environment variable
    - Moved Test helpers into own folder for reusability

---
+++ test/helpers/wait-for-update.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+
+// helper for async assertions.
+// Use like this:
+//
+// vm.a = 123
+// waitForUpdate(() => {
+//   expect(vm.$el.textContent).toBe('123')
+//   vm.a = 234
+// })
+// .then(() => {
+//   // more assertions...
+//   done()
+// })
+// .catch(done)
+window.waitForUpdate = initialCb => {
+  let done
+  const queue = [initialCb]
+
+  function shift () {
+    const job = queue.shift()
+    let hasError = false
+    try {
+      job()
+    } catch (e) {
+      hasError = true
+      if (done) {
+        done.fail(e)
+      }
+    }
+    if (!hasError) {
+      if (queue.length) {
+        Vue.nextTick(shift)
+      }
+    }
+  }
+
+  Vue.nextTick(shift)
+
+  const chainer = {
+    then: nextCb => {
+      queue.push(nextCb)
+      return chainer
+    },
+    catch: doneCb => {
+      done = doneCb
+      return chainer
+    }
+  }
+
+  return chainer
+}
---
+++ test/ssr/ssr-env.spec.js
@@ -0,0 +1,45 @@
+import Vue from '../../dist/vue.common.js'
+import createRenderer from '../../dist/server-renderer.js'
+import '../helpers/to-have-been-warned.js'
+
+describe('SSR: VUE_ENV=server', () => {
+  it('_isServer set as "server" on Vue config', () => {
+    expect(Vue.config._isServer).toBe(true)
+  })
+
+  it('$isServer set as true on VM', () => {
+    const vm = new Vue({
+      data: {
+        foo: 'server',
+        bar: 'rendering'
+      }
+    })
+    expect(vm.$isServer).toBe(true)
+  })
+
+  it('no data observations', () => {
+    const vm = new Vue({
+      data: {
+        foo: 'server',
+        bar: 'rendering'
+      },
+      computed: {
+        combined () {
+          return this.foo + this.bar
+        }
+      }
+    })
+
+    vm.foo = ''
+    expect(vm.foo).toBe('')
+    expect(vm.combined).toBe('rendering')
+    expect(vm.$data.__ob__).toBe(undefined)
+  })
+
+  it('should warn when not set', () => {
+    process.env.VUE_ENV = ''
+    createRenderer()
+    expect('You are using createRenderer without setting VUE_ENV enviroment').toHaveBeenWarned()
+    process.env.VUE_ENV = 'server'
+  })
+})
