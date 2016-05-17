39e42f503c26e7cdc539ca6a2e0f8403a821cc9a
Evan You <yyx990803@gmail.com>
2016-05-17 10:37:20 -0400

    add vm annotation in init

--- src/core/instance/init.js
+++ src/core/instance/init.js
@@ -11,7 +11,7 @@ let uid = 0
 
 export function initMixin (Vue: Class<Component>) {
   Vue.prototype._init = function (options?: Object) {
-    const vm = this
+    const vm: Component = this
     // a uid
     vm._uid = uid++
     // a flag to avoid this being observed
