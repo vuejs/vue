fd6944a597daa14589e123a5b132d8645b1dfa18
Evan You <yyx990803@gmail.com>
2016-05-16 21:03:02 -0400

    small tweak for coverage

--- src/platforms/web/compiler/directives/text.js
+++ src/platforms/web/compiler/directives/text.js
@@ -3,6 +3,7 @@
 import { addProp } from 'compiler/helpers'
 
 export default function text (el: ASTElement, dir: ASTDirective) {
-  if (!dir.value) return
-  addProp(el, 'textContent', `__toString__(${dir.value})`)
+  if (dir.value) {
+    addProp(el, 'textContent', `__toString__(${dir.value})`)
+  }
 }
