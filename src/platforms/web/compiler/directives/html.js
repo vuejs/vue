39f28ddecf247f7ae171fc98c5341496916da44e
Evan You <yyx990803@gmail.com>
2016-05-16 21:03:02 -0400

    small tweak for coverage

--- src/platforms/web/compiler/directives/html.js
+++ src/platforms/web/compiler/directives/html.js
@@ -3,6 +3,7 @@
 import { addProp } from 'compiler/helpers'
 
 export default function html (el: ASTElement, dir: ASTDirective) {
-  if (!dir.value) return
-  addProp(el, 'innerHTML', `__toString__(${dir.value})`)
+  if (dir.value) {
+    addProp(el, 'innerHTML', `__toString__(${dir.value})`)
+  }
 }
