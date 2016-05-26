746b3e5e42c6de1b6d247fa1ac1cdc8545d6444f
Evan You <yyx990803@gmail.com>
2016-05-26 11:32:52 -0400

    improve ci configuration

--- build/ci.sh
+++ build/ci.sh
@@ -1,6 +1,5 @@
 set -e
-if [ -z "$CI_PULL_REQUEST" ]
-then
+if [ -z "$CI_PULL_REQUEST" ] || [ "$CIRCLE_BRANCH" == "master" ]; then
   npm run lint
   npm run cover
   cat ./coverage/lcov.info | ./node_modules/.bin/codecov
