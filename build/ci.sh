8785b79e2fb4ddb3d840ba4edbb82e6e0165b7a8
Evan You <yyx990803@gmail.com>
2016-06-13 18:19:20 -0400

    adjust ci script

--- build/ci.sh
+++ build/ci.sh
@@ -1,5 +1,10 @@
 set -e
-npm run ci
+npm run lint
+npm run flow
+npm run test:cover
+npm run test:e2e -- --env phantomjs
+npm run test:ssr
 if [ -z "$CI_PULL_REQUEST" ]; then
   cat ./coverage/lcov.info | ./node_modules/.bin/codecov
+  npm run test:sauce
 fi
