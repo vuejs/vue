3245c72314fe0bd869338804dd7f8b63d8a92f5f
Evan You <yyx990803@gmail.com>
2016-06-25 08:14:16 -0400

    fix ssr dev config

--- build/webpack.ssr.dev.config.js
+++ build/webpack.ssr.dev.config.js
@@ -4,7 +4,6 @@ var alias = require('./alias')
 module.exports = {
   target: 'node',
   entry: path.resolve(__dirname, 'webpack.ssr.dev.entry.js'),
-  target: 'node',
   output: {
     path: path.resolve(__dirname, '../packages/vue-server-renderer'),
     filename: 'create-renderer.js',
