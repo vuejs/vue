1d3503b5aa6aedf68669ab30140890b2dd51df59
tommyZZM <tommyZZM@users.noreply.github.com>
2016-07-11 22:57:19 +0800

    add "aside" as reservedTag

--- src/platforms/web/util/element.js
+++ src/platforms/web/util/element.js
@@ -10,7 +10,7 @@ export const namespaceMap = {
 
 export const isReservedTag = makeMap(
   'html,body,base,head,link,meta,style,title,' +
-  'address,article,footer,header,h1,h2,h3,h4,h5,h6,hgroup,nav,section,' +
+  'address,article,aside,footer,header,h1,h2,h3,h4,h5,h6,hgroup,nav,section,' +
   'div,dd,dl,dt,figcaption,figure,hr,img,li,main,ol,p,pre,ul,' +
   'a,b,abbr,bdi,bdo,br,cite,code,data,dfn,em,i,kbd,mark,q,rp,rt,rtc,ruby,' +
   's,samp,small,span,strong,sub,sup,time,u,var,wbr,area,audio,map,track,video,' +
