0f8ad06b2c580fd9e484b2d559a59fc06914f22a
Brandon Surowiec <BrandonSurowiec@users.noreply.github.com>
2016-08-22 17:03:00 -0400

    Fix Spelling (#3496)
    * Fix Spelling
    
    * Fix other instances.

--- src/compiler/parser/index.js
+++ src/compiler/parser/index.js
@@ -90,7 +90,7 @@ export function parse (
       if (process.env.VUE_ENV !== 'server' && isForbiddenTag(element)) {
         element.forbidden = true
         process.env.NODE_ENV !== 'production' && warn(
-          'Templates should only be responsbile for mapping the state to the ' +
+          'Templates should only be responsible for mapping the state to the ' +
           'UI. Avoid placing tags with side-effects in your templates, such as ' +
           `<${tag}>.`
         )
