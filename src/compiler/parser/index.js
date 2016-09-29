82983231084d8649efd0eef9d613aa3e36f912da
Evan You <yyx990803@gmail.com>
2016-09-29 13:00:47 -0400

    add <template> key warning

--- src/compiler/parser/index.js
+++ src/compiler/parser/index.js
@@ -115,12 +115,12 @@ export function parse (
         processFor(element)
         processIf(element)
         processOnce(element)
+        processKey(element)
 
         // determine whether this is a plain element after
         // removing structural attributes
         element.plain = !element.key && !attrs.length
 
-        processKey(element)
         processRef(element)
         processSlot(element)
         processComponent(element)
@@ -258,6 +258,9 @@ function processRawAttrs (el) {
 function processKey (el) {
   const exp = getBindingAttr(el, 'key')
   if (exp) {
+    if (process.env.NODE_ENV !== 'production' && el.tag === 'template') {
+      warn(`<template> cannot be keyed. Place the key on real elements instead.`)
+    }
     el.key = exp
   }
 }
