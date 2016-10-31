b320ae3b2ada80c3927ac7d0c8a41b73450f7051
Herrington Darkholme <nonamesheep1@gmail.com>
2016-10-31 23:06:36 +0800

    fix #4055, generate style on custom component

--- src/platforms/web/server/modules/style.js
+++ src/platforms/web/server/modules/style.js
@@ -2,24 +2,43 @@
 
 import { hyphenate, toObject } from 'shared/util'
 
-export default function renderStyle (node: VNodeWithData): ?string {
+function concatStyleString (former: string, latter: string) {
+  if (former === '' || latter === '' || former.charAt(former.length - 1) === ';') {
+    return former + latter
+  }
+  return former + ';' + latter
+}
+
+function generateStyleText (node) {
   const staticStyle = node.data.attrs && node.data.attrs.style
-  if (node.data.style || staticStyle) {
-    let styles = node.data.style
-    let res = ''
-    if (styles) {
-      if (typeof styles === 'string') {
-        res += styles
-      } else {
-        if (Array.isArray(styles)) {
-          styles = toObject(styles)
-        }
-        for (const key in styles) {
-          res += `${hyphenate(key)}:${styles[key]};`
-        }
-        res += staticStyle || ''
+  let styles = node.data.style
+  const parentStyle = node.parent ? generateStyleText(node.parent) : ''
+
+  if (!styles && !staticStyle) {
+    return parentStyle
+  }
+
+  let dynamicStyle = ''
+  if (styles) {
+    if (typeof styles === 'string') {
+      dynamicStyle += styles
+    } else {
+      if (Array.isArray(styles)) {
+        styles = toObject(styles)
+      }
+      for (const key in styles) {
+        dynamicStyle += `${hyphenate(key)}:${styles[key]};`
       }
     }
+  }
+
+  dynamicStyle = concatStyleString(parentStyle, dynamicStyle)
+  return concatStyleString(dynamicStyle, staticStyle || '')
+}
+
+export default function renderStyle (node: VNodeWithData): ?string {
+  const res = generateStyleText(node)
+  if (res) {
     return ` style=${JSON.stringify(res)}`
   }
 }
