72dcdd8d39656b2c2e2dc7ea4f223e8d59622460
Victor Zakharenko <ponymtb@gmail.com>
2016-11-03 06:52:47 +0300

    Revert "package vue-template-compiler update"
    This reverts commit 0104458d631115d1abf8d7a6a694a1d84b9b0fc5.

--- packages/vue-template-compiler/build.js
+++ packages/vue-template-compiler/build.js
@@ -5140,9 +5140,6 @@ function parseComponent (
       if (attr.name === 'scoped') {
         block.scoped = true;
       }
-      if (attr.name === 'module') {
-        block.module = attr.value || true;
-      }
       if (attr.name === 'src') {
         block.src = attr.value;
       }
