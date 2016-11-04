199833a915056534bd225d9a9a4a671565fcf450
Sean MacIsaac <seanjmacisaac@gmail.com>
2016-11-04 08:09:20 -0400

    Changed 'Vue.createElement' to be a pointer to the function

--- src/core/global-api/createElement.js
+++ src/core/global-api/createElement.js
@@ -4,7 +4,5 @@ import { createElement } from '../vdom/create-element'
 
 // Using the spread operator to avoid code duplication from the origin
 export function initCreateElement (Vue: GlobalAPI) {
-  Vue.createElement = function (...args): VNode | void {
-    return createElement(...args)
-  }
+  Vue.createElement = createElement
 }
