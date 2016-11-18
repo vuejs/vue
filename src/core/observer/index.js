cbc1fbc3c72984dd331bb6cad5a3102f64a241ff
Evan You <yyx990803@gmail.com>
2016-11-18 12:34:01 -0500

    fix eslint no-self-compare

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -162,9 +162,11 @@ export function defineReactive (
     },
     set: function reactiveSetter (newVal) {
       const value = getter ? getter.call(obj) : val
+      /* eslint-disable no-self-compare */
       if (newVal === value || (newVal !== newVal && value !== value)) {
         return
       }
+      /* eslint-enable no-self-compare */
       if (process.env.NODE_ENV !== 'production' && customSetter) {
         customSetter()
       }
