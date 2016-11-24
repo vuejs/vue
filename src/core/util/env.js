e89830230c5747fbe1b70bb4e6c423c3ec2934cc
Evan You <yyx990803@gmail.com>
2016-11-24 11:03:27 -0500

    fix browser env detection for jsdom in Node 7

--- src/core/util/env.js
+++ src/core/util/env.js
@@ -7,10 +7,7 @@ import { noop } from 'shared/util'
 export const hasProto = '__proto__' in {}
 
 // Browser environment sniffing
-export const inBrowser =
-  typeof window !== 'undefined' &&
-  Object.prototype.toString.call(window) !== '[object Object]'
-
+export const inBrowser = typeof window !== 'undefined'
 export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
 export const isIE = UA && /msie|trident/.test(UA)
 export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
