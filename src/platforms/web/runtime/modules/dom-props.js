d88100e1a679d53b2bd1501e48165fbf52f9befb
defcc <chengchao1@gmail.com>
2016-12-17 10:51:01 +0800

    update dom prop for option.value

--- src/platforms/web/runtime/modules/dom-props.js
+++ src/platforms/web/runtime/modules/dom-props.js
@@ -37,7 +37,8 @@ function updateDOMProps (oldVnode: VNodeWithData, vnode: VNodeWithData) {
       const strCur = cur == null ? '' : String(cur)
       if (!elm.composing && (
         (document.activeElement !== elm && elm.value !== strCur) ||
-        isValueChanged(vnode, strCur)
+        isValueChanged(vnode, strCur) ||
+        elm.tagName.toLowerCase() === 'option'
       )) {
         elm.value = strCur
       }
