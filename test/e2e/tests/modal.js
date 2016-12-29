9c108afd859228f2e1d3bb1216a4988f0b7c60fb
miherlosev@mail.ru <Ein7Onai>
2016-12-29 12:27:31 +0300

    run testcafe tests on https://circleci.com

--- test/e2e/tests/modal.js
+++ test/e2e/tests/modal.js
@@ -10,9 +10,9 @@ test('modal', async t => {
     await t
         .click(showModalBtn)
         .expect(modal.mainElement.exists).ok()
+        .expect(modal.mainElement.hasClass('modal-enter-active')).ok()
         .expect(modal.wrapper.exists).ok()
         .expect(modal.container.exists).ok()
-        .expect(modal.mainElement.hasClass('modal-enter-active')).ok()
         .expect(modal.mainElement.hasClass('modal-enter-active')).notOk()
         .expect(modal.header.find('h3').innerText).contains('custom header')
         .expect(modal.body.innerText).contains('default body')
