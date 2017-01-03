6f9d43815ca3103c697b98d4aa5ee6eec96ec1ca
defcc <chengchao1@gmail.com>
2017-01-03 23:46:42 +0800

    listen to click event for checkbox and radio.

--- src/platforms/web/compiler/directives/model.js
+++ src/platforms/web/compiler/directives/model.js
@@ -59,7 +59,7 @@ function genCheckboxModel (
       `?_i(${value},${valueBinding})>-1` +
       `:_q(${value},${trueValueBinding})`
   )
-  addHandler(el, 'change',
+  addHandler(el, 'click',
     `var $$a=${value},` +
         '$$el=$event.target,' +
         `$$c=$$el.checked?(${trueValueBinding}):(${falseValueBinding});` +
@@ -90,7 +90,7 @@ function genRadioModel (
   let valueBinding = getBindingAttr(el, 'value') || 'null'
   valueBinding = number ? `_n(${valueBinding})` : valueBinding
   addProp(el, 'checked', `_q(${value},${valueBinding})`)
-  addHandler(el, 'change', genAssignmentCode(value, valueBinding), null, true)
+  addHandler(el, 'click', genAssignmentCode(value, valueBinding), null, true)
 }
 
 function genDefaultModel (
