74b2acbab11c7cd5c8cb99921f2b31e64e54e246
cygeng <cygeng@cam2cloud.com>
2017-01-05 17:32:35 +0800

    Multiple elements event handler with .once modifier no removal bug fixed

--- src/platforms/web/runtime/modules/events.js
+++ src/platforms/web/runtime/modules/events.js
@@ -2,26 +2,26 @@
 
 import { updateListeners } from 'core/vdom/helpers/index'
 
-let target: HTMLElement
-
-function add (event: string, handler: Function, once: boolean, capture: boolean) {
-  if (once) {
-    const oldHandler = handler
-    handler = function (ev) {
-      remove(event, handler, capture)
-      arguments.length === 1
-        ? oldHandler(ev)
-        : oldHandler.apply(null, arguments)
-    }
-  }
-  target.addEventListener(event, handler, capture)
-}
-
-function remove (event: string, handler: Function, capture: boolean) {
-  target.removeEventListener(event, handler, capture)
-}
-
 function updateDOMListeners (oldVnode: VNodeWithData, vnode: VNodeWithData) {
+  let target: HTMLElement
+
+  function add (event: string, handler: Function, once: boolean, capture: boolean) {
+    if (once) {
+      const oldHandler = handler
+      handler = function (ev) {
+        remove(event, handler, capture)
+        arguments.length === 1
+          ? oldHandler(ev)
+          : oldHandler.apply(null, arguments)
+      }
+    }
+    target.addEventListener(event, handler, capture)
+  }
+
+  function remove (event: string, handler: Function, capture: boolean) {
+    target.removeEventListener(event, handler, capture)
+  }
+
   if (!oldVnode.data.on && !vnode.data.on) {
     return
   }
