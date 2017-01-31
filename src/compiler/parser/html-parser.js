d14bd641430d936867a5d289d513a37917a0deb6
jddxf <740531372@qq.com>
2017-02-01 01:03:49 +0800

    Avoid double-decoding on attribute value (#4797)
    * Use a single `replace` instead so that we could avoid the double-decoding issue,i.e. get `"` after decoding `&amp;quot;` while `&quot;` is expected.
    
    * Don't instantiate RegExp on every call

--- src/compiler/parser/html-parser.js
+++ src/compiler/parser/html-parser.js
@@ -49,21 +49,19 @@ let IS_REGEX_CAPTURING_BROKEN = false
 const isScriptOrStyle = makeMap('script,style', true)
 const reCache = {}
 
-const ltRE = /&lt;/g
-const gtRE = /&gt;/g
-const nlRE = /&#10;/g
-const ampRE = /&amp;/g
-const quoteRE = /&quot;/g
+const decodingMap = {
+  '&lt;': '<',
+  '&gt;': '>',
+  '&quot;': '"',
+  '&amp;': '&',
+  '&#10;': '\n'
+}
+const encodedAttr = /&(lt|gt|quot|amp);/g
+const encodedAttrWithNewLines = /&(lt|gt|quot|amp|#10);/g
 
 function decodeAttr (value, shouldDecodeNewlines) {
-  if (shouldDecodeNewlines) {
-    value = value.replace(nlRE, '\n')
-  }
-  return value
-    .replace(ltRE, '<')
-    .replace(gtRE, '>')
-    .replace(ampRE, '&')
-    .replace(quoteRE, '"')
+  const re = shouldDecodeNewlines ? encodedAttrWithNewLines : encodedAttr
+  return value.replace(re, match => decodingMap[match])
 }
 
 export function parseHTML (html, options) {
