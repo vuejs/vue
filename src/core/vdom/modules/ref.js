acec8db2c4b54ac6f9cda7ab8fcc3bdd213eb3ed
Rahul Kadyan <hi@znck.me>
2017-02-16 03:59:13 +0530

    [WIP] Support for ref callback (#4807)
    * :sparkles: Tests for ref callback
    
    * :sparkles: Support for ref callback
    
    * Add test of inline ref callback
    
    * adjust ref implementation strategy
    
    * fix patch ref registration
    
    * fix tests
    
    * fix flow
    
    * fix test for phantomjs

--- src/core/vdom/modules/ref.js
+++ src/core/vdom/modules/ref.js
@@ -1,44 +1,40 @@
 /* @flow */
 
-import { remove } from 'shared/util'
-
 export default {
-  create (_: any, vnode: VNodeWithData) {
-    registerRef(vnode)
-  },
-  update (oldVnode: VNodeWithData, vnode: VNodeWithData) {
-    if (oldVnode.data.ref !== vnode.data.ref) {
-      registerRef(oldVnode, true)
-      registerRef(vnode)
-    }
-  },
-  destroy (vnode: VNodeWithData) {
-    registerRef(vnode, true)
-  }
+  create: registerRef,
+  update: registerRef
 }
 
-export function registerRef (vnode: VNodeWithData, isRemoval: ?boolean) {
+export function registerRef (_: any, vnode: VNodeWithData) {
   const key = vnode.data.ref
   if (!key) return
 
-  const vm = vnode.context
   const ref = vnode.componentInstance || vnode.elm
-  const refs = vm.$refs
-  if (isRemoval) {
-    if (Array.isArray(refs[key])) {
-      remove(refs[key], ref)
-    } else if (refs[key] === ref) {
-      refs[key] = undefined
-    }
-  } else {
-    if (vnode.data.refInFor) {
-      if (Array.isArray(refs[key]) && refs[key].indexOf(ref) < 0) {
-        refs[key].push(ref)
-      } else {
-        refs[key] = [ref]
+  const refs = vnode.context.$refs
+
+  if (typeof key === 'function') {
+    key(ref)
+  } else if (vnode.data.refInFor) {
+    const refArray = refs[key]
+    if (Array.isArray(refArray)) {
+      if (refArray.indexOf(ref) < 0) {
+        refArray.push(ref)
       }
     } else {
-      refs[key] = ref
+      refs[key] = [ref]
+    }
+  } else {
+    refs[key] = ref
+  }
+}
+
+export function resetRefs (refs: Refs): Refs {
+  const res = {}
+  // keep existing v-for ref arrays even if empty
+  for (const key in refs) {
+    if (Array.isArray(refs[key])) {
+      res[key] = []
     }
   }
+  return res
 }
