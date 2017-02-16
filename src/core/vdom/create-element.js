61e1943dc78f8611bcfaed62906f157eefd5cabf
Steve Shreeve <steve.shreeve@gmail.com>
2017-02-16 13:13:50 -0700

    Allow for custom createElement function, using "Vue.createElement = myCustomCreateElement"

--- src/core/vdom/create-element.js
+++ src/core/vdom/create-element.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+import Vue from '../instance/index'
 import VNode, { createEmptyVNode } from './vnode'
 import config from '../config'
 import { createComponent } from './create-component'
@@ -19,6 +20,10 @@ export function createElement (
   normalizationType: any,
   alwaysNormalize: boolean
 ): VNode {
+  if (Vue.createElement) {
+    Vue._createElement = _createElement // TODO: do this only once
+    return Vue.createElement.apply(this, arguments)
+  }
   if (Array.isArray(data) || isPrimitive(data)) {
     normalizationType = children
     children = data
