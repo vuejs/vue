10b4b4ddbdc2dcc8df88961d2622c89a6927cacb
Evan You <yyx990803@gmail.com>
2017-02-25 17:13:41 -0500

    support ES2015 Symbol in provide/inject

--- src/core/instance/inject.js
+++ src/core/instance/inject.js
@@ -1,5 +1,9 @@
 /* @flow */
 
+import { isNative } from 'core/util/env'
+
+const hasReflect = typeof Reflect !== 'undefined' && isNative(Reflect.ownKeys)
+
 export function initInjections (vm: Component) {
   const provide = vm.$options.provide
   const inject: any = vm.$options.inject
@@ -12,7 +16,12 @@ export function initInjections (vm: Component) {
     // inject is :any because flow is not smart enough to figure out cached
     // isArray here
     const isArray = Array.isArray(inject)
-    const keys = isArray ? inject : Object.keys(inject)
+    const keys = isArray
+      ? inject
+      : hasReflect
+        ? Reflect.ownKeys(inject)
+        : Object.keys(inject)
+
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i]
       const provideKey = isArray ? key : inject[key]
