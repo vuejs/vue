2f751b5d9908968549e3aa05d546d3b5055110b1
kazuya kawaguchi <kawakazu80@gmail.com>
2017-02-28 02:18:18 +0900

    refactor argument name (#5026)
    NOTE:
      related docs: https://github.com/vuejs/vuejs.org/pull/787
      adjust to flowtype definition
      https://github.com/vuejs/vue/blob/dev/src/core/config.js#L12

--- src/core/util/error.js
+++ src/core/util/error.js
@@ -2,12 +2,12 @@ import config from '../config'
 import { warn } from './debug'
 import { inBrowser } from './env'
 
-export function handleError (err, vm, type) {
+export function handleError (err, vm, info) {
   if (config.errorHandler) {
-    config.errorHandler.call(null, err, vm, type)
+    config.errorHandler.call(null, err, vm, info)
   } else {
     if (process.env.NODE_ENV !== 'production') {
-      warn(`Error in ${type}:`, vm)
+      warn(`Error in ${info}:`, vm)
     }
     /* istanbul ignore else */
     if (inBrowser && typeof console !== 'undefined') {
