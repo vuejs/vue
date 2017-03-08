0265a532dc4a9e24f3e4a3194279f7b03b883fe2
Romoo <romoochen+git@gmail.com>
2017-03-08 13:33:34 +0800

    remove useless console.log

--- benchmarks/ssr/renderToString.js
+++ benchmarks/ssr/renderToString.js
@@ -11,5 +11,4 @@ self.s = self.performance.now()
 
 renderToString(new Vue(gridComponent), () => {
   console.log('Complete time: ' + (self.performance.now() - self.s).toFixed(2) + 'ms')
-  console.log()
 })
