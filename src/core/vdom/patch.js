4d48082762764b02e38330742ed80f7ea0a7c373
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-04-03 06:36:50 +0300

    fix #5321 don't throw error when node gets relocated (#5322)
    * don't throw error when node gets relocated
    
    * perf: Simplify if check in vdom/patch

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -234,7 +234,9 @@ export function createPatchFunction (backend) {
   function insert (parent, elm, ref) {
     if (isDef(parent)) {
       if (isDef(ref)) {
-        nodeOps.insertBefore(parent, elm, ref)
+        if (ref.parentNode === parent) {
+          nodeOps.insertBefore(parent, elm, ref)
+        }
       } else {
         nodeOps.appendChild(parent, elm)
       }
