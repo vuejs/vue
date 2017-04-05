534191667cb67c25bce22c819f1d21a51740c69e
Evan You <yyx990803@gmail.com>
2017-04-05 13:04:07 +0800

    split get-first-component-child

--- src/core/vdom/helpers/index.js
+++ src/core/vdom/helpers/index.js
@@ -5,7 +5,4 @@ export * from './extract-props'
 export * from './update-listeners'
 export * from './normalize-children'
 export * from './resolve-async-component'
-
-export function getFirstComponentChild (children: ?Array<VNode>): ?VNode {
-  return children && children.filter((c: VNode) => c && c.componentOptions)[0]
-}
+export * from './get-first-component-child'
