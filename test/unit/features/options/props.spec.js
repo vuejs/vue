682141fccc9b3224c3432afcd5befd23fba9b1a1
Evan You <yyx990803@gmail.com>
2017-04-10 20:36:59 +0800

    support Symbol in props type validation (close #5396)

--- test/unit/features/options/props.spec.js
+++ test/unit/features/options/props.spec.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { hasSymbol } from 'core/util/env'
 
 describe('Options props', () => {
   it('array syntax', done => {
@@ -204,6 +205,15 @@ describe('Options props', () => {
       makeInstance({}, Array)
       expect('Expected Array').toHaveBeenWarned()
     })
+
+    if (hasSymbol) {
+      it('symbol', () => {
+        makeInstance(Symbol('foo'), Symbol)
+        expect(console.error.calls.count()).toBe(0)
+        makeInstance({}, Symbol)
+        expect('Expected Symbol').toHaveBeenWarned()
+      })
+    }
 
     it('custom constructor', () => {
       function Class () {}
