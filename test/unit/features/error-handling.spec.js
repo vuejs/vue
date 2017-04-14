8d7072d80e2ab96a478a0566f055ee9da8280882
Evan You <yyx990803@gmail.com>
2017-04-14 15:19:41 +0800

    test warning component trace

--- test/unit/features/error-handling.spec.js
+++ test/unit/features/error-handling.spec.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import { formatComponentName } from 'core/util/debug'
 
 const components = createErrorTestComponents()
 
@@ -123,28 +122,6 @@ describe('Error handling', () => {
         done()
       })
     })
-  })
-
-  it('properly format component names', () => {
-    const vm = new Vue()
-    expect(formatComponentName(vm)).toBe('<Root>')
-
-    vm.$root = null
-    vm.$options.name = 'hello-there'
-    expect(formatComponentName(vm)).toBe('<HelloThere>')
-
-    vm.$options.name = null
-    vm.$options._componentTag = 'foo-bar-1'
-    expect(formatComponentName(vm)).toBe('<FooBar1>')
-
-    vm.$options._componentTag = null
-    vm.$options.__file = '/foo/bar/baz/SomeThing.vue'
-    expect(formatComponentName(vm)).toBe(`<SomeThing> at ${vm.$options.__file}`)
-    expect(formatComponentName(vm, false)).toBe('<SomeThing>')
-
-    vm.$options.__file = 'C:\\foo\\bar\\baz\\windows_file.vue'
-    expect(formatComponentName(vm)).toBe(`<WindowsFile> at ${vm.$options.__file}`)
-    expect(formatComponentName(vm, false)).toBe('<WindowsFile>')
   })
 })
 
