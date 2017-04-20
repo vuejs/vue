a677a98a3c2a4780ba07e58dbba614bd9277d3d3
Evan You <yyx990803@gmail.com>
2017-04-20 15:26:00 +0800

    update outdated message in ssr plugin

--- src/server/webpack-plugin/util.js
+++ src/server/webpack-plugin/util.js
@@ -1,4 +1,4 @@
-const { red, yellow, gray } = require('chalk')
+const { red, yellow } = require('chalk')
 
 const prefix = `[vue-server-renderer-webpack-plugin]`
 const warn = exports.warn = msg => console.error(red(`${prefix} ${msg}\n`))
@@ -15,9 +15,8 @@ export const validate = compiler => {
 
   if (!compiler.options.externals) {
     tip(
-      'It is recommended to externalize dependencies for better ssr performance.\n' +
-      `See ${gray('https://github.com/vuejs/vue/tree/dev/packages/vue-server-renderer#externals')} ` +
-      'for more details.'
+      'It is recommended to externalize dependencies in the server build for ' +
+      'better build performance.'
     )
   }
 }
