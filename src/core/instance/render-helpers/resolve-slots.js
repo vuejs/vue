38516b494293a9ee9bae697d559107341dca89dd
Evan You <yyx990803@gmail.com>
2017-04-25 12:28:20 +0800

    fix flow

--- src/core/instance/render-helpers/resolve-slots.js
+++ src/core/instance/render-helpers/resolve-slots.js
@@ -12,13 +12,13 @@ export function resolveSlots (
     return slots
   }
   const defaultSlot = []
-  let name, child
   for (let i = 0, l = children.length; i < l; i++) {
-    child = children[i]
+    const child = children[i]
     // named slots should only be respected if the vnode was rendered in the
     // same context.
     if ((child.context === context || child.functionalContext === context) &&
-        child.data && (name = child.data.slot) != null) {
+        child.data && child.data.slot != null) {
+      const name = child.data.slot
       const slot = (slots[name] || (slots[name] = []))
       if (child.tag === 'template') {
         slot.push.apply(slot, child.children)
