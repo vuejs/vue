e83f1e43dbeedb0df8845aa2e2e09dde2c1afd7f
Eduardo San Martin Morote <posva13@gmail.com>
2017-05-03 16:11:27 +0200

    Prevent unecessary input trigger with v-model
    Fix #5586

--- src/platforms/web/runtime/directives/model.js
+++ src/platforms/web/runtime/directives/model.js
@@ -119,6 +119,8 @@ function onCompositionStart (e) {
 }
 
 function onCompositionEnd (e) {
+  // prevent triggering an input event for no reason
+  if (!e.target.composing) return
   e.target.composing = false
   trigger(e.target, 'input')
 }
