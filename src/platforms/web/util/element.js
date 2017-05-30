14ca1a5c5daec96dec20d63556ac8581efcaa752
guang <ylg@meitu.com>
2017-05-30 11:09:41 +0800

    fix: review

--- src/platforms/web/util/element.js
+++ src/platforms/web/util/element.js
@@ -19,8 +19,7 @@ export const isHTMLTag = makeMap(
   'button,datalist,fieldset,form,input,label,legend,meter,optgroup,option,' +
   'output,progress,select,textarea,' +
   'details,dialog,menu,menuitem,summary,' +
-  'content,element,shadow,template,' +
-  'big,blockquote,iframe,keygen,tfoot'
+  'content,element,shadow,template,blockquote,iframe,tfoot'
 )
 
 // this map is intentionally selective, only covering SVG elements that may
@@ -28,8 +27,7 @@ export const isHTMLTag = makeMap(
 export const isSVG = makeMap(
   'svg,animate,circle,clippath,cursor,defs,desc,ellipse,filter,font-face,' +
   'foreignObject,g,glyph,image,line,marker,mask,missing-glyph,path,pattern,' +
-  'polygon,polyline,rect,switch,symbol,text,textpath,tspan,use,view,' +
-  'clipPath,linearGradient,radialGradient,stop',
+  'polygon,polyline,rect,switch,symbol,text,textpath,tspan,use,view',
   true
 )
 
