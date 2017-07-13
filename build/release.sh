529e3db6ae0298ae2450cab47fb16c06699caa44
Evan You <yyx990803@gmail.com>
2017-07-13 13:57:16 +0800

    build: update build script

--- build/release.sh
+++ build/release.sh
@@ -49,14 +49,13 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   git add -A
   git add -f \
     dist/*.js \
-    !dist/vue.common.min.js \
     packages/vue-server-renderer/basic.js \
     packages/vue-server-renderer/build.js \
     packages/vue-server-renderer/server-plugin.js \
     packages/vue-server-renderer/client-plugin.js \
     packages/vue-template-compiler/build.js
-  git commit -m "[build] $VERSION"
-  npm version $VERSION --message "[release] $VERSION"
+  git commit -m "build: build $VERSION"
+  npm version $VERSION --message "build: release $VERSION"
 
   # publish
   git push origin refs/tags/v$VERSION
@@ -66,7 +65,4 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   else
     npm publish --tag $RELEASE_TAG
   fi
-
-  # generate release note
-  VERSION=$VERSION npm run release:note
 fi
