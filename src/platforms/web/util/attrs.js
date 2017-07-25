79e24682d12f1e5b439b65de08e4d83dfc1856db
gebilaoxiong <gebilaoxiong@gmail.com>
2017-07-25 13:39:53 +0800

    chore(runtime): correctly render the embed attribute allowfullscreen
    #6202

--- src/platforms/web/util/attrs.js
+++ src/platforms/web/util/attrs.js
@@ -17,10 +17,12 @@ export const mustUseProp = (tag: string, type: ?string, attr: string): boolean =
   )
 }
 
-export const isEnumeratedAttr = makeMap('contenteditable,draggable,spellcheck')
+export const isEnumeratedAttr = makeMap(
+  'allowfullscreen,contenteditable,draggable,spellcheck'
+)
 
 export const isBooleanAttr = makeMap(
-  'allowfullscreen,async,autofocus,autoplay,checked,compact,controls,declare,' +
+  'async,autofocus,autoplay,checked,compact,controls,declare,' +
   'default,defaultchecked,defaultmuted,defaultselected,defer,disabled,' +
   'enabled,formnovalidate,hidden,indeterminate,inert,ismap,itemscope,loop,multiple,' +
   'muted,nohref,noresize,noshade,novalidate,nowrap,open,pauseonexit,readonly,' +
