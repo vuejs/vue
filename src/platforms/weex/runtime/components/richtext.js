f4fe7d04b70f72e128db65cd4289dacae8f08abf
Hanks <zhanghan.me@gmail.com>
2017-07-25 17:03:18 +0800

    feat(weex richtext): support events and add more test cases

--- src/platforms/weex/runtime/components/richtext.js
+++ src/platforms/weex/runtime/components/richtext.js
@@ -1,9 +1,8 @@
 function getVNodeType (vnode) {
-  const tagName = vnode.tag
-  if (!tagName) {
+  if (!vnode.tag) {
     return ''
   }
-  return tagName.replace(/vue\-component\-(\d+\-)?/, '')
+  return vnode.tag.replace(/vue\-component\-(\d+\-)?/, '')
 }
 
 function isSimpleSpan (vnode) {
@@ -11,10 +10,15 @@ function isSimpleSpan (vnode) {
 }
 
 function trimCSSUnit (prop) {
+  // TODO: more reliable
   return Number(String(prop).replace(/px$/i, '')) || prop
 }
 
 function parseStyle (vnode) {
+  if (!vnode || !vnode.data) {
+    return
+  }
+
   const { staticStyle, staticClass } = vnode.data
   if (vnode.data.style || vnode.data.class || staticStyle || staticClass) {
     const styles = Object.assign({}, staticStyle, vnode.data.style)
@@ -39,6 +43,7 @@ function convertVNodeChildren (children) {
   if (!children.length) {
     return
   }
+
   return children.map(vnode => {
     const type = getVNodeType(vnode)
     const props = { type }
@@ -49,17 +54,20 @@ function convertVNodeChildren (children) {
       props.attr = {
         value: (vnode.text || '').trim()
       }
-    }
-
-    if (vnode.data) {
+    } else {
       props.style = parseStyle(vnode)
-      props.attr = vnode.data.attrs
-    }
+      if (vnode.data) {
+        props.attr = vnode.data.attrs
+        if (vnode.data.on) {
+          props.events = vnode.data.on
+        }
+      }
 
-    if (type === 'span' && isSimpleSpan(vnode)) {
-      props.attr = props.attr || {}
-      props.attr.value = vnode.children[0].text.trim()
-      return props
+      if (type === 'span' && isSimpleSpan(vnode)) {
+        props.attr = props.attr || {}
+        props.attr.value = vnode.children[0].text.trim()
+        return props
+      }
     }
 
     if (vnode.children && vnode.children.length) {
@@ -72,9 +80,10 @@ function convertVNodeChildren (children) {
 
 export default {
   name: 'richtext',
-  abstract: true,
+  // abstract: true,
   render (h) {
     return h('weex:richtext', {
+      on: this._events,
       attrs: {
         value: convertVNodeChildren(this.$options._renderChildren || [])
       }
