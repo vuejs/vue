66689c28a1b7a1f7470d6a2e1264b9dec5a22f70
strandjun <strand.jun@gmail.com>
2017-08-07 23:51:28 +0800

    update `&&`

--- src/core/global-api/assets.js
+++ src/core/global-api/assets.js
@@ -17,9 +17,9 @@ export function initAssetRegisters (Vue: GlobalAPI) {
         return this.options[type + 's'][id]
       }
       /* istanbul ignore if */
-      if (process.env.NODE_ENV !== 'production'
-          && type === 'component'
-          && config.isReservedTag(id)) {
+      if (process.env.NODE_ENV !== 'production' && 
+          type === 'component' && 
+          config.isReservedTag(id)) {
           warn('Do not use built-in or reserved HTML elements as component id: ' + id)
       }
       if (type === 'component' && isPlainObject(definition)) {
