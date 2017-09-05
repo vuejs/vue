89f0d29f2d541aa5a1ac9690258cd7c7ee576ef6
AchillesJ <xujiongbo@gmail.com>
2017-09-06 03:12:35 +0800

    fix: inherit SVG ns on component root node (#6511)
    fix #6506

--- test/unit/modules/vdom/create-element.spec.js
+++ test/unit/modules/vdom/create-element.spec.js
@@ -141,6 +141,24 @@ describe('create-element', () => {
     expect(vnode.children[0].children[0].ns).toBeUndefined()
   })
 
+  // #6506
+  it('render SVGAElement in a component correctly', () => {
+    const vm = new Vue({
+      template: `
+        <svg>
+          <test></test>
+        </svg>
+      `,
+      components: {
+        test: { render: h => h('a') }
+      }
+    }).$mount()
+    const testComp = vm.$children[0]
+    expect(testComp.$vnode.ns).toBe('svg')
+    expect(testComp._vnode.tag).toBe('a')
+    expect(testComp._vnode.ns).toBe('svg')
+  })
+
   it('warn observed data objects', () => {
     new Vue({
       data: {
