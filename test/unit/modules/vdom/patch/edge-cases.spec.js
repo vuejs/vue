a7444975343f7828004d90bfb0deeb98db0f46e7
Evan You <yyx990803@gmail.com>
2017-09-05 11:29:22 -0400

    fix: ensure outer bindings on nested HOC are properly re-applied on inner root element change

--- test/unit/modules/vdom/patch/edge-cases.spec.js
+++ test/unit/modules/vdom/patch/edge-cases.spec.js
@@ -161,4 +161,32 @@ describe('vdom patch: edge cases', () => {
       expect(vm.$el.children[0].type).toBe('password')
     }).then(done)
   })
+
+  it('should properly patch nested HOC when root element is replaced', done => {
+    const vm = new Vue({
+      template: `<foo class="hello" ref="foo" />`,
+      components: {
+        foo: {
+          template: `<bar ref="bar" />`,
+          components: {
+            bar: {
+              template: `<div v-if="ok"></div><span v-else></span>`,
+              data () {
+                return { ok: true }
+              }
+            }
+          }
+        }
+      }
+    }).$mount()
+
+    expect(vm.$refs.foo.$refs.bar.$el.tagName).toBe('DIV')
+    expect(vm.$refs.foo.$refs.bar.$el.className).toBe(`hello`)
+
+    vm.$refs.foo.$refs.bar.ok = false
+    waitForUpdate(() => {
+      expect(vm.$refs.foo.$refs.bar.$el.tagName).toBe('SPAN')
+      expect(vm.$refs.foo.$refs.bar.$el.className).toBe(`hello`)
+    }).then(done)
+  })
 })
