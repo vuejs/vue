32dc3e9072acb9cb2921dee28b6a904e634c15ae
Evan You <yyx990803@gmail.com>
2017-09-06 05:48:01 -0400

    refactor: fix types

--- src/platforms/weex/compiler/modules/recycle-list/index.js
+++ src/platforms/weex/compiler/modules/recycle-list/index.js
@@ -33,9 +33,11 @@ function postTransformNode (el: ASTElement) {
 function genText (node) {
   const value = node.type === 3
     ? node.text
-    : node.tokens.length === 1
-      ? node.tokens[0]
-      : node.tokens
+    : node.type === 2
+      ? node.tokens.length === 1
+        ? node.tokens[0]
+        : node.tokens
+      : ''
   return JSON.stringify(value)
 }
 
