5933652554bf415faca5d248938d80061539cc04
Hanks <zhanghan.me@gmail.com>
2017-09-16 22:43:36 +0800

    feat($compiler): compile weex native directives in preTransformNode

--- src/platforms/weex/compiler/modules/recycle-list/v-if.js
+++ src/platforms/weex/compiler/modules/recycle-list/v-if.js
@@ -1,29 +1,13 @@
 /* @flow */
 
-import { getAndRemoveAttr, addAttr } from 'compiler/helpers'
-
-function isConditionAttr (name: string): boolean {
-  return /^v\-if|v\-else|v\-else\-if/.test(name)
-}
+import { getAndRemoveAttr } from 'compiler/helpers'
 
 export function transformVIf (el: ASTElement) {
-  for (const attr in el.attrsMap) {
-    if (!isConditionAttr(attr)) {
-      continue
-    }
-    const binding = getAndRemoveAttr(el, attr)
-    switch (attr) {
-      case 'v-if': {
-        addAttr(el, '[[match]]', binding)
-        el.attrsMap['[[match]]'] = binding
-        el.attrsList.push({ name: '[[match]]', value: binding })
-        delete el.attrsMap[attr]
-        delete el.if
-        delete el.ifConditions
-        break
-      }
-
-      // TODO: support v-else and v-else-if
-    }
+  const exp = getAndRemoveAttr(el, 'v-if')
+  if (exp) {
+    el.attrsMap['[[match]]'] = exp
+    el.attrsList.push({ name: '[[match]]', value: exp })
+    delete el.attrsMap['v-if']
   }
+  // TODO: support v-else and v-else-if
 }
