c7b57e1769784d8d65fbded901735723d0607b46
Hanks <zhanghan.me@gmail.com>
2017-09-16 14:59:23 +0800

    feat(compile): supports compiling v-if to the weex native directive

--- flow/compiler.js
+++ flow/compiler.js
@@ -72,8 +72,8 @@ declare type ASTNode = ASTElement | ASTText | ASTExpression;
 declare type ASTElement = {
   type: 1;
   tag: string;
-  attrsList: Array<{ name: string; value: string }>;
-  attrsMap: { [key: string]: string | null };
+  attrsList: Array<{ name: string; value: any }>;
+  attrsMap: { [key: string]: any };
   parent: ASTElement | void;
   children: Array<ASTNode>;
 
@@ -84,7 +84,7 @@ declare type ASTElement = {
   hasBindings?: boolean;
 
   text?: string;
-  attrs?: Array<{ name: string; value: string }>;
+  attrs?: Array<{ name: string; value: any }>;
   props?: Array<{ name: string; value: string }>;
   plain?: boolean;
   pre?: true;
