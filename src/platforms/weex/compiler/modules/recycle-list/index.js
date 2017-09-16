4a306b88d5f6a6feff657d032d52e701e5efc785
Hanks <zhanghan.me@gmail.com>
2017-09-17 00:04:05 +0800

    feat($compiler): supports compiling v-else-if and v-else to the weex native directive

--- src/platforms/weex/compiler/modules/recycle-list/index.js
+++ src/platforms/weex/compiler/modules/recycle-list/index.js
@@ -7,15 +7,15 @@ import { transformVFor } from './v-for'
 
 let currentRecycleList = null
 
-function preTransformNode (el: ASTElement) {
+function preTransformNode (el: ASTElement, options: CompilerOptions) {
   if (el.tag === 'recycle-list') {
     currentRecycleList = el
   }
   if (currentRecycleList) {
     // TODO
     transformVBind(el)
-    transformVIf(el)
-    transformVFor(el)
+    transformVIf(el, options) // and v-else-if and v-else
+    transformVFor(el, options)
   }
 }
 
--- src/platforms/weex/compiler/modules/recycle-list/v-if.js
+++ src/platforms/weex/compiler/modules/recycle-list/v-if.js
@@ -2,12 +2,47 @@
 
 import { getAndRemoveAttr } from 'compiler/helpers'
 
-export function transformVIf (el: ASTElement) {
-  const exp = getAndRemoveAttr(el, 'v-if')
-  if (exp) {
+function hasConditionDirective (el: ASTElement): boolean {
+  for (const attr in el.attrsMap) {
+    if (/^v\-if|v\-else|v\-else\-if$/.test(attr)) {
+      return true
+    }
+  }
+  return false
+}
+
+function getPrevMatch (el: ASTElement): any {
+  if (el.parent && el.parent.children) {
+    const prev: Object = el.parent.children[el.parent.children.length - 1]
+    return prev.attrsMap['[[match]]']
+  }
+}
+
+export function transformVIf (el: ASTElement, options: CompilerOptions) {
+  if (hasConditionDirective(el)) {
+    let exp
+    const ifExp = getAndRemoveAttr(el, 'v-if')
+    const elseifExp = getAndRemoveAttr(el, 'v-else-if')
+    if (ifExp) {
+      exp = ifExp
+    } else {
+      const prevMatch = getPrevMatch(el)
+      if (prevMatch) {
+        exp = elseifExp
+          ? `!(${prevMatch}) && (${elseifExp})` // v-else-if
+          : `!(${prevMatch})` // v-else
+      } else if (process.env.NODE_ENV !== 'production' && options.warn) {
+        options.warn(
+          `v-${elseifExp ? ('else-if="' + elseifExp + '"') : 'else'} ` +
+          `used on element <${el.tag}> without corresponding v-if.`
+        )
+        return
+      }
+    }
     el.attrsMap['[[match]]'] = exp
     el.attrsList.push({ name: '[[match]]', value: exp })
     delete el.attrsMap['v-if']
+    delete el.attrsMap['v-else-if']
+    delete el.attrsMap['v-else']
   }
-  // TODO: support v-else and v-else-if
 }
