8f0e46cecce81fc3954c077b0c5c41429f580ec3
Evan You <yyx990803@gmail.com>
2017-10-02 17:11:36 -0400

    fix: work around old Chrome bug
    fix #6601

--- src/platforms/web/runtime/modules/dom-props.js
+++ src/platforms/web/runtime/modules/dom-props.js
@@ -28,6 +28,11 @@ function updateDOMProps (oldVnode: VNodeWithData, vnode: VNodeWithData) {
     if (key === 'textContent' || key === 'innerHTML') {
       if (vnode.children) vnode.children.length = 0
       if (cur === oldProps[key]) continue
+      // #6601 work around Chrome version <= 55 bug where single textNode
+      // replaced by innerHTML/textContent retains its parentNode property
+      if (elm.childNodes.length === 1) {
+        elm.removeChild(elm.childNodes[0])
+      }
     }
 
     if (key === 'value') {
