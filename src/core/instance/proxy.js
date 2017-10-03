8d9b626b722fed9c858750ea63fbe1e6cacce2d8
Flavio Corpa <flaviocorpa@gmail.com>
2017-10-03 17:45:38 +0200

    refactor: improve error msg for non-reactive properties (#6735)
    close #6657

--- src/core/instance/proxy.js
+++ src/core/instance/proxy.js
@@ -15,9 +15,11 @@ if (process.env.NODE_ENV !== 'production') {
 
   const warnNonPresent = (target, key) => {
     warn(
-      `Property or method "${key}" is not defined on the instance but ` +
-      `referenced during render. Make sure to declare reactive data ` +
-      `properties in the data option.`,
+      `Property or method "${key}" is not defined on the instance but` +
+      'referenced during render. Make sure that this property is reactive, ' +
+      'either in the data option, or for class-based components, by ' +
+      'initializing the property.' +
+      'See: https://vuejs.org/v2/guide/reactivity.html#Declaring-Reactive-Properties.',
       target
     )
   }
