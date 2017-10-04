2431d3d74396b33a2a120a835cfe7a776f06e277
Evan You <yyx990803@gmail.com>
2017-10-03 22:25:52 -0400

    chore: fix warning space

--- src/core/instance/proxy.js
+++ src/core/instance/proxy.js
@@ -15,10 +15,10 @@ if (process.env.NODE_ENV !== 'production') {
 
   const warnNonPresent = (target, key) => {
     warn(
-      `Property or method "${key}" is not defined on the instance but` +
+      `Property or method "${key}" is not defined on the instance but ` +
       'referenced during render. Make sure that this property is reactive, ' +
       'either in the data option, or for class-based components, by ' +
-      'initializing the property.' +
+      'initializing the property. ' +
       'See: https://vuejs.org/v2/guide/reactivity.html#Declaring-Reactive-Properties.',
       target
     )
