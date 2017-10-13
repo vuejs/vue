11c527bd9c826ec95ac383f6dd9658a047a1d57a
Evan You <yyx990803@gmail.com>
2017-10-13 15:37:52 -0400

    fix: further adjust nextTick strategy
    fix #6813

--- src/core/util/env.js
+++ src/core/util/env.js
@@ -1,7 +1,4 @@
 /* @flow */
-/* globals MessageChannel */
-
-import { handleError } from './error'
 
 // can we use __proto__?
 export const hasProto = '__proto__' in {}
@@ -61,88 +58,6 @@ export function isNative (Ctor: any): boolean {
 export const hasSymbol =
   typeof Symbol !== 'undefined' && isNative(Symbol) &&
   typeof Reflect !== 'undefined' && isNative(Reflect.ownKeys)
-
-/**
- * Defer a task to execute it asynchronously.
- */
-export const nextTick = (function () {
-  const callbacks = []
-  let pending = false
-  let timerFunc
-
-  function nextTickHandler () {
-    pending = false
-    const copies = callbacks.slice(0)
-    callbacks.length = 0
-    for (let i = 0; i < copies.length; i++) {
-      copies[i]()
-    }
-  }
-
-  // An asynchronous deferring mechanism.
-  // In pre 2.4, we used to use microtasks (Promise/MutationObserver)
-  // but microtasks actually has too high a priority and fires in between
-  // supposedly sequential events (e.g. #4521, #6690) or even between
-  // bubbling of the same event (#6566). Technically setImmediate should be
-  // the ideal choice, but it's not available everywhere; and the only polyfill
-  // that consistently queues the callback after all DOM events triggered in the
-  // same loop is by using MessageChannel.
-  /* istanbul ignore if */
-  if (typeof setImmediate !== 'undefined' && isNative(setImmediate)) {
-    timerFunc = () => {
-      setImmediate(nextTickHandler)
-    }
-  } else if (typeof MessageChannel !== 'undefined' && (
-    isNative(MessageChannel) ||
-    // PhantomJS
-    MessageChannel.toString() === '[object MessageChannelConstructor]'
-  )) {
-    const channel = new MessageChannel()
-    const port = channel.port2
-    channel.port1.onmessage = nextTickHandler
-    timerFunc = () => {
-      port.postMessage(1)
-    }
-  } else
-  /* istanbul ignore next */
-  if (typeof Promise !== 'undefined' && isNative(Promise)) {
-    // use microtask in non-DOM environments, e.g. Weex
-    const p = Promise.resolve()
-    timerFunc = () => {
-      p.then(nextTickHandler)
-    }
-  } else {
-    // fallback to setTimeout
-    timerFunc = () => {
-      setTimeout(nextTickHandler, 0)
-    }
-  }
-
-  return function queueNextTick (cb?: Function, ctx?: Object) {
-    let _resolve
-    callbacks.push(() => {
-      if (cb) {
-        try {
-          cb.call(ctx)
-        } catch (e) {
-          handleError(e, ctx, 'nextTick')
-        }
-      } else if (_resolve) {
-        _resolve(ctx)
-      }
-    })
-    if (!pending) {
-      pending = true
-      timerFunc()
-    }
-    // $flow-disable-line
-    if (!cb && typeof Promise !== 'undefined') {
-      return new Promise((resolve, reject) => {
-        _resolve = resolve
-      })
-    }
-  }
-})()
 
 let _Set
 /* istanbul ignore if */ // $flow-disable-line
