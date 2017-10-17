34814e832118a6f681a02e74afb3906a76facd81
马金花儿 <o.o@mug.dog>
2017-10-17 06:24:39 -0500

    Take a few change

--- test/unit/features/directives/once.spec.js
+++ test/unit/features/directives/once.spec.js
@@ -17,7 +17,7 @@ describe('Directive v-once', () => {
     const list = ['foo', 'bar']
     const MyComponent = Vue.extend({
       props: ['item'],
-      template: '<div v-once>{{ item }}</div>'
+      template: '<li v-once>{{ item }}</li>'
     })
     const vm = new Vue({
       components: {
