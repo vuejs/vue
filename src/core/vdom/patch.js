836fc5fa1c6ccf8f69ac17bb5ad7a0f1351198b4
cxtom <chenxiao07@baidu.com>
2017-10-26 12:03:40 +0800

    fix(patch): fix child node create failed when hydration failed

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -107,6 +107,7 @@ export function createPatchFunction (backend) {
   function createElm (vnode, insertedVnodeQueue, parentElm, refElm, nested) {
     vnode.isRootInsert = !nested // for transition enter check
     if (createComponent(vnode, insertedVnodeQueue, parentElm, refElm)) {
+      insert(parentElm, vnode.elm, refElm)
       return
     }
 
