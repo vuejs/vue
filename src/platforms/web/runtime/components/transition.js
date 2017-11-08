2b4cadbf74bb781d45d1aa253177e7adb455b039
Richard Musiol <mail@richard-musiol.de>
2017-11-08 17:58:32 +0100

    fix out-in transition getting stuck with v-if
    An out-in transition could get stuck if the child with v-if was not yet shown and then replaced with a different child. In this case, "afterLeave" would never get fired, thus "_leaving" stayed true indefinitely. This was likely to happen with vue-router and asynchronous data loading before the new page is shown.
    
    Fixes #6687.

--- src/platforms/web/runtime/components/transition.js
+++ src/platforms/web/runtime/components/transition.js
@@ -160,6 +160,7 @@ export default {
     if (
       oldChild &&
       oldChild.data &&
+      oldChild.elm.nodeType !== Node.COMMENT_NODE &&
       !isSameChild(child, oldChild) &&
       !isAsyncPlaceholder(oldChild)
     ) {
