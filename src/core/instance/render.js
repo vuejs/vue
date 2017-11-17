c71f4e92bba2b1b440b96a39c0d6846b62c8b168
Evan You <yyx990803@gmail.com>
2017-11-17 09:45:03 -0500

    fix: init _staticTrees to avoid runtime reference warning
    fix #7075

--- src/core/instance/render.js
+++ src/core/instance/render.js
@@ -17,6 +17,7 @@ import { isUpdatingChildComponent } from './lifecycle'
 
 export function initRender (vm: Component) {
   vm._vnode = null // the root of the child tree
+  vm._staticTrees = null // v-once cached trees
   const options = vm.$options
   const parentVnode = vm.$vnode = options._parentVnode // the placeholder node in parent tree
   const renderContext = parentVnode && parentVnode.context
