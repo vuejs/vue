452c1679b8c5d5c7d316c8c93101839f0881f546
Hanks <zhanghan.me@gmail.com>
2017-11-23 09:42:07 +0800

    test(weex): add more test cases for recycle-list (#7104)

---
+++ test/weex/cases/recycle-list/components/stateful.vdom.js
@@ -0,0 +1,45 @@
+({
+  type: 'recycle-list',
+  attr: {
+    listData: [
+      { type: 'A', number: 24 },
+      { type: 'A', number: 42 }
+    ],
+    templateKey: 'type',
+    alias: 'item'
+  },
+  children: [{
+    type: 'cell-slot',
+    attr: { templateType: 'A' },
+    children: [{
+      type: 'div',
+      attr: {
+        '@isComponentRoot': true,
+        '@componentProps': {
+          start: { '@binding': 'item.number' }
+        }
+      },
+      children: [{
+        type: 'text',
+        style: { fontSize: '150px', textAlign: 'center' },
+        attr: {
+          value: { '@binding': 'count' } // need confirm
+        }
+      }, {
+        type: 'text',
+        event: ['click'],
+        style: {
+          fontSize: '100px',
+          textAlign: 'center',
+          borderWidth: '2px',
+          borderColor: '#DDDDDD',
+          backgroundColor: '#F5F5F5'
+        },
+        attr: { value: '+' }
+      }]
+    }, {
+      type: 'text',
+      attr: { value: 'other' }
+    }]
+  }]
+})
