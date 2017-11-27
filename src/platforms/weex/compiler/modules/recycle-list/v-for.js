5b43b449d42ac979da2fedb56b36836ce1f6e89e
Evan You <yyx990803@gmail.com>
2017-11-27 17:48:46 -0500

    fix(weex): update recycle-list v-for transform

--- src/platforms/weex/compiler/modules/recycle-list/v-for.js
+++ src/platforms/weex/compiler/modules/recycle-list/v-for.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import { forAliasRE, forIteratorRE } from 'compiler/parser/index'
+import { forAliasRE, forIteratorRE, stripParensRE } from 'compiler/parser/index'
 import { getAndRemoveAttr } from 'compiler/helpers'
 
 export function preTransformVFor (el: ASTElement, options: CompilerOptions) {
@@ -10,18 +10,19 @@ export function preTransformVFor (el: ASTElement, options: CompilerOptions) {
   }
   const inMatch = exp.match(forAliasRE)
   if (inMatch) {
-    const alias = inMatch[1].trim()
+    const alias = inMatch[1].trim().replace(stripParensRE, '')
     const desc: Object = {
       '@expression': inMatch[2].trim(),
       '@alias': alias
     }
     const iteratorMatch = alias.match(forIteratorRE)
     if (iteratorMatch) {
-      desc['@alias'] = iteratorMatch[1].trim()
-      desc['@index'] = iteratorMatch[2].trim()
-      if (iteratorMatch[3]) {
-        desc['@key'] = iteratorMatch[2].trim()
-        desc['@index'] = iteratorMatch[3].trim()
+      desc['@alias'] = alias.replace(forIteratorRE, '')
+      if (iteratorMatch[2]) {
+        desc['@key'] = iteratorMatch[1].trim()
+        desc['@index'] = iteratorMatch[2].trim()
+      } else {
+        desc['@index'] = iteratorMatch[1].trim()
       }
     }
     delete el.attrsMap['v-for']
