7388789e118e880423a5870e9b312d720b74316c
Jason <jason.daurus@gmail.com>
2017-11-28 21:39:43 +0800

    feat(compiler): Fix warning in directives/on

--- src/compiler/directives/on.js
+++ src/compiler/directives/on.js
@@ -1,11 +1,10 @@
 /* @flow */
 
 import { warn } from 'core/util/index'
-import { getRawAttr } from '../helpers'
 
 export default function on (el: ASTElement, dir: ASTDirective) {
   if (process.env.NODE_ENV !== 'production' && dir.modifiers) {
-    warn(`v-on without argument does not support modifiers.`, getRawAttr(el, 'v-on'))
+    warn(`v-on without argument does not support modifiers.`)
   }
   el.wrapListeners = (code: string) => `_g(${code},${dir.value})`
 }
