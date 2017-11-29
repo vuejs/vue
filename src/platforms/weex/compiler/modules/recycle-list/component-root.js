27d11a45e1f8ad497f2be7c64b4cd032859c9664
Evan You <yyx990803@gmail.com>
2017-11-29 15:44:06 -0500

    feat(weex): WIP adjust component transform stage

--- src/platforms/weex/compiler/modules/recycle-list/component-root.js
+++ src/platforms/weex/compiler/modules/recycle-list/component-root.js
@@ -1,14 +1,14 @@
 /* @flow */
 
-import { addRawAttr } from 'compiler/helpers'
+import { addAttr } from 'compiler/helpers'
 
 // mark component root nodes as
-export function preTransformComponentRoot (
+export function postTransformComponentRoot (
   el: ASTElement,
   options: WeexCompilerOptions
 ) {
   if (!el.parent) {
     // component root
-    addRawAttr(el, '$isComponentRoot', true)
+    addAttr(el, '@isComponentRoot', 'true')
   }
 }
