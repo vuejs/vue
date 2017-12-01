94f9176553d27339f665eddb806511ab740d8633
Hanks <zhanghan.me@gmail.com>
2017-12-01 15:42:22 +0800

    test(weex): add more component test cases for recycle-list

---
+++ test/weex/cases/recycle-list/components/stateful-v-model.vue
@@ -0,0 +1,21 @@
+<template>
+  <recycle-list :list-data="longList" template-key="type" alias="item">
+    <cell-slot template-type="A">
+      <editor message="No binding"></editor>
+    </cell-slot>
+  </recycle-list>
+</template>
+
+<script>
+  // require('./editor.vue')
+  module.exports = {
+    data () {
+      return {
+        longList: [
+          { type: 'A' },
+          { type: 'A' }
+        ]
+      }
+    }
+  }
+</script>
