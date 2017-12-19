77b4e08e124ea02a9599bdb6a083ae56e25eeac1
Herrington Darkholme <nonamesheep1@gmail.com>
2017-12-19 11:06:55 +0800

    feat(warning): allow symbol as vdom key
    fix #7157

--- src/shared/util.js
+++ src/shared/util.js
@@ -27,6 +27,8 @@ export function isPrimitive (value: any): boolean %checks {
   return (
     typeof value === 'string' ||
     typeof value === 'number' ||
+    // $flow-disable-line
+    typeof value === 'symbol' ||
     typeof value === 'boolean'
   )
 }
