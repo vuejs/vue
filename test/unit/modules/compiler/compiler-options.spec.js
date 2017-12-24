ea15272169135e6e405b88fbcdb757909df15600
Huo Chunyang <HcySunYang@outlook.com>
2017-12-25 01:28:28 +0800

    chore: correct the eslint script statement in package.json and correct the format. (#7308)

--- test/unit/modules/compiler/compiler-options.spec.js
+++ test/unit/modules/compiler/compiler-options.spec.js
@@ -9,58 +9,58 @@ describe('compile options', () => {
         <input type="text" v-model="msg" required max="8" v-validate:field1.group1.group2>
       </div>
     `, {
-        directives: {
-          validate (el, dir) {
-            if (dir.name === 'validate' && dir.arg) {
-              el.validate = {
-                field: dir.arg,
-                groups: dir.modifiers ? Object.keys(dir.modifiers) : []
-              }
+      directives: {
+        validate (el, dir) {
+          if (dir.name === 'validate' && dir.arg) {
+            el.validate = {
+              field: dir.arg,
+              groups: dir.modifiers ? Object.keys(dir.modifiers) : []
             }
           }
+        }
+      },
+      modules: [{
+        transformNode (el) {
+          el.validators = el.validators || []
+          const validators = ['required', 'min', 'max', 'pattern', 'maxlength', 'minlength']
+          validators.forEach(name => {
+            const rule = getAndRemoveAttr(el, name)
+            if (rule !== undefined) {
+              el.validators.push({ name, rule })
+            }
+          })
         },
-        modules: [{
-          transformNode (el) {
-            el.validators = el.validators || []
-            const validators = ['required', 'min', 'max', 'pattern', 'maxlength', 'minlength']
-            validators.forEach(name => {
-              const rule = getAndRemoveAttr(el, name)
-              if (rule !== undefined) {
-                el.validators.push({ name, rule })
-              }
-            })
-          },
-          genData (el) {
-            let data = ''
-            if (el.validate) {
-              data += `validate:${JSON.stringify(el.validate)},`
-            }
-            if (el.validators) {
-              data += `validators:${JSON.stringify(el.validators)},`
-            }
-            return data
-          },
-          transformCode (el, code) {
-          // check
-            if (!el.validate || !el.validators) {
-              return code
-            }
-            // setup validation result props
-            const result = { dirty: false } // define something other prop
-            el.validators.forEach(validator => {
-              result[validator.name] = null
-            })
-            // generate code
-            return `_c('validate',{props:{
-            field:${JSON.stringify(el.validate.field)},
-            groups:${JSON.stringify(el.validate.groups)},
-            validators:${JSON.stringify(el.validators)},
-            result:${JSON.stringify(result)},
-            child:${code}}
-          })`
+        genData (el) {
+          let data = ''
+          if (el.validate) {
+            data += `validate:${JSON.stringify(el.validate)},`
           }
-        }]
-      })
+          if (el.validators) {
+            data += `validators:${JSON.stringify(el.validators)},`
+          }
+          return data
+        },
+        transformCode (el, code) {
+        // check
+          if (!el.validate || !el.validators) {
+            return code
+          }
+          // setup validation result props
+          const result = { dirty: false } // define something other prop
+          el.validators.forEach(validator => {
+            result[validator.name] = null
+          })
+          // generate code
+          return `_c('validate',{props:{
+          field:${JSON.stringify(el.validate.field)},
+          groups:${JSON.stringify(el.validate.groups)},
+          validators:${JSON.stringify(el.validators)},
+          result:${JSON.stringify(result)},
+          child:${code}}
+        })`
+        }
+      }]
+    })
     expect(render).not.toBeUndefined()
     expect(staticRenderFns).toEqual([])
     expect(errors).toEqual([])
