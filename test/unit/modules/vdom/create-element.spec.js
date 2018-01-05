7969c80d7934a1ed22cf0d38d27a27bd529b20f6
AchillesJ <xujiongbo@gmail.com>
2018-01-06 06:44:44 +0800

    fix(vdom): svg inside foreignObject should be rendered with correct namespace (fix #7330) (#7350)
    * add failed test case
    
    * fix failed test case
    
    * fix(vdom): svg inside foreignObject should be rendered with correct namespace
    
    * adjust comments

--- test/unit/modules/vdom/create-element.spec.js
+++ test/unit/modules/vdom/create-element.spec.js
@@ -135,10 +135,12 @@ describe('create-element', () => {
   it('render svg foreignObject with correct namespace', () => {
     const vm = new Vue({})
     const h = vm.$createElement
-    const vnode = h('svg', [h('foreignObject', [h('p')])])
+    const vnode = h('svg', [h('foreignObject', [h('p'), h('svg')])])
     expect(vnode.ns).toBe('svg')
     expect(vnode.children[0].ns).toBe('svg')
     expect(vnode.children[0].children[0].ns).toBeUndefined()
+    // #7330
+    expect(vnode.children[0].children[1].ns).toBe('svg')
   })
 
   // #6642
