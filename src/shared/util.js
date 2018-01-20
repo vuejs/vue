46965bed990888f0d6ea11e14c202d4c05f10ad3
dmitry-vakhnenko <33278950+dmitry-vakhnenko@users.noreply.github.com>
2018-01-21 02:23:15 +0300

    use bind polyfill if Function.bind not exits

--- src/shared/util.js
+++ src/shared/util.js
@@ -177,6 +177,21 @@ export const hyphenate = cached((str: string): string => {
  * Bind funciton context
  */
 export function bind (fn: Function, ctx: Object): Function {
+  if(!Function.prototype.bind) {
+    function boundFn (a) {
+      const l = arguments.length
+      return l
+        ? l > 1
+          ? fn.apply(ctx, arguments)
+          : fn.call(ctx, a)
+        : fn.call(ctx)
+    }
+    // record original fn length
+    boundFn._length = fn.length
+    
+    return boundFn
+  }
+  
   return fn.bind(ctx)
 }
 
