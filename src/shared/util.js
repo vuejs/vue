8c23f992309eda75c0b914d9da6660a117cd393d
dmitry-vakhnenko <33278950+dmitry-vakhnenko@users.noreply.github.com>
2018-01-21 00:59:16 +0300

    Use native bind
    Simple refactor bind function for easy revert.
    Custom bind implementation performance is lower than native.
    
    https://jsperf.com/vue-bind-perf

--- src/shared/util.js
+++ src/shared/util.js
@@ -174,20 +174,10 @@ export const hyphenate = cached((str: string): string => {
 })
 
 /**
- * Simple bind, faster than native
+ * Bind funciton context
  */
 export function bind (fn: Function, ctx: Object): Function {
-  function boundFn (a) {
-    const l: number = arguments.length
-    return l
-      ? l > 1
-        ? fn.apply(ctx, arguments)
-        : fn.call(ctx, a)
-      : fn.call(ctx)
-  }
-  // record original fn length
-  boundFn._length = fn.length
-  return boundFn
+  return fn.bind(ctx)
 }
 
 /**
