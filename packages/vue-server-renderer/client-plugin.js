d67ade7d49efdbcb6391ee091c03898d8cf5516e
Emily Fueger <efueger@users.noreply.github.com>
2018-01-23 12:37:55 -0500

    Update client-plugin.js

--- packages/vue-server-renderer/client-plugin.js
+++ packages/vue-server-renderer/client-plugin.js
@@ -31,42 +31,7 @@ VueSSRClientPlugin.prototype.apply = function apply (compiler) {
     var allFiles = uniq(stats.assets
       .map(function (a) { return a.name; }));
 
-    var initialFiles = uniq(Object.keys(stats.entrypoints)
-      .map(function (name) { return stats.entrypoints[name].assets; })
-      .reduce(function (assets, all) { return all.concat(assets); }, [])
-      .filter(isJS));
-
-    var asyncFiles = allFiles
-      .filter(isJS)
-      .filter(function (file) { return initialFiles.indexOf(file) < 0; });
-
-    var manifest = {
-      publicPath: stats.publicPath,
-      all: allFiles,
-      initial: initialFiles,
-      async: asyncFiles,
-      modules: { /* [identifier: string]: Array<index: number> */ }
-    };
-
-    var assetModules = stats.modules.filter(function (m) { return m.assets.length; });
-    var fileToIndex = function (file) { return manifest.all.indexOf(file); };
-    stats.modules.forEach(function (m) {
-      // ignore modules duplicated in multiple chunks
-      if (m.chunks.length === 1) {
-        var cid = m.chunks[0];
-        var chunk = stats.chunks.find(function (c) { return c.id === cid; });
-        if (!chunk || !chunk.files) {
-          return
-        }
-        var files = manifest.modules[hash(m.identifier)] = chunk.files.map(fileToIndex);
-        // find all asset modules associated with the same chunk
-        assetModules.forEach(function (m) {
-          if (m.chunks.some(function (id) { return id === cid; })) {
-            files.push.apply(files, m.assets.map(fileToIndex));
-          }
-        });
-      }
-    });
+    
 
     // const debug = (file, obj) => {
     // require('fs').writeFileSync(__dirname + '/' + file, JSON.stringify(obj, null, 2))
