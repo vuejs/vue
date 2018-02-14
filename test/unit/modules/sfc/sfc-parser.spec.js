4627f29829eff9e47ed8e0facf9d9160d8fc914c
ktsn <ktsn55@gmail.com>
2018-02-14 23:46:07 +0900

    fix(sfc): avoid to deindent when pad option is specified

--- test/unit/modules/sfc/sfc-parser.spec.js
+++ test/unit/modules/sfc/sfc-parser.spec.js
@@ -71,21 +71,33 @@ describe('Single File Component parser', () => {
     const padLine = parseComponent(content.trim(), { pad: 'line' })
     const padSpace = parseComponent(content.trim(), { pad: 'space' })
 
-    expect(padDefault.script.content).toBe(Array(3 + 1).join('//\n') + '\nexport default {}\n')
-    expect(padDefault.styles[0].content).toBe(Array(6 + 1).join('\n') + '\nh1 { color: red }\n')
-    expect(padLine.script.content).toBe(Array(3 + 1).join('//\n') + '\nexport default {}\n')
-    expect(padLine.styles[0].content).toBe(Array(6 + 1).join('\n') + '\nh1 { color: red }\n')
+    expect(padDefault.script.content).toBe(Array(3 + 1).join('//\n') + `
+        export default {}
+      `)
+    expect(padDefault.styles[0].content).toBe(Array(6 + 1).join('\n') + `
+        h1 { color: red }
+      `)
+    expect(padLine.script.content).toBe(Array(3 + 1).join('//\n') + `
+        export default {}
+      `)
+    expect(padLine.styles[0].content).toBe(Array(6 + 1).join('\n') + `
+        h1 { color: red }
+      `)
     expect(padSpace.script.content).toBe(`<template>
         <div></div>
       </template>
-      <script>`.replace(/./g, ' ') + '\nexport default {}\n')
+      <script>`.replace(/./g, ' ') + `
+        export default {}
+      `)
     expect(padSpace.styles[0].content).toBe(`<template>
         <div></div>
       </template>
       <script>
         export default {}
       </script>
-      <style>`.replace(/./g, ' ') + '\nh1 { color: red }\n')
+      <style>`.replace(/./g, ' ') + `
+        h1 { color: red }
+      `)
   })
 
   it('should handle template blocks with lang as special text', () => {
