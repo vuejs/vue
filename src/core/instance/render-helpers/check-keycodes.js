f7311c90b35da6ee680dacb0566a276bff907ccb
Evan You <yyx990803@gmail.com>
2018-03-07 14:50:35 -0500

    refactor: move keyName alias map to compiler

--- src/core/instance/render-helpers/check-keycodes.js
+++ src/core/instance/render-helpers/check-keycodes.js
@@ -3,18 +3,6 @@
 import config from 'core/config'
 import { hyphenate } from 'shared/util'
 
-const keyNames: { [key: string]: string | Array<string> } = {
-  esc: 'Escape',
-  tab: 'Tab',
-  enter: 'Enter',
-  space: ' ',
-  up: 'ArrowUp',
-  left: 'ArrowLeft',
-  right: 'ArrowRight',
-  down: 'ArrowDown',
-  'delete': ['Backspace', 'Delete']
-}
-
 function isKeyNotMatch<T> (expect: T | Array<T>, actual: T): boolean {
   if (Array.isArray(expect)) {
     return expect.indexOf(actual) === -1
@@ -31,15 +19,15 @@ function isKeyNotMatch<T> (expect: T | Array<T>, actual: T): boolean {
 export function checkKeyCodes (
   eventKeyCode: number,
   key: string,
-  builtInAlias?: number | Array<number>,
-  eventKeyName?: string
+  builtInKeyCode?: number | Array<number>,
+  eventKeyName?: string,
+  builtInKeyName?: string | Array<string>
 ): ?boolean {
-  const keyCodes = config.keyCodes[key] || builtInAlias
-  const builtInName: string | Array<string> = keyNames[key]
-  if (builtInName && keyCodes === builtInAlias && eventKeyName) {
-    return isKeyNotMatch(builtInName, eventKeyName)
-  } else if (keyCodes) {
-    return isKeyNotMatch(keyCodes, eventKeyCode)
+  const mappedKeyCode = config.keyCodes[key] || builtInKeyCode
+  if (builtInKeyName && eventKeyName && !config.keyCodes[key]) {
+    return isKeyNotMatch(builtInKeyName, eventKeyName)
+  } else if (mappedKeyCode) {
+    return isKeyNotMatch(mappedKeyCode, eventKeyCode)
   } else if (eventKeyName) {
     return hyphenate(eventKeyName) !== key
   }
