c3cbf79211d64c2c47acfb5e1f6b0c28211019a2
JK <jingkai.zhao@foxmail.com>
2018-03-08 03:38:41 +0800

    fix: support KeyboardEvent.key in built-in keyboard event modifiers (#7121)
    fix #6900

--- src/core/instance/render-helpers/check-keycodes.js
+++ src/core/instance/render-helpers/check-keycodes.js
@@ -3,6 +3,26 @@
 import config from 'core/config'
 import { hyphenate } from 'shared/util'
 
+const keyNames: { [key: string]: string | Array<string> } = {
+  esc: 'Escape',
+  tab: 'Tab',
+  enter: 'Enter',
+  space: ' ',
+  up: 'ArrowUp',
+  left: 'ArrowLeft',
+  right: 'ArrowRight',
+  down: 'ArrowDown',
+  'delete': ['Backspace', 'Delete']
+}
+
+function isKeyNotMatch<T> (expect: T | Array<T>, actual: T): boolean {
+  if (Array.isArray(expect)) {
+    return expect.indexOf(actual) === -1
+  } else {
+    return expect !== actual
+  }
+}
+
 /**
  * Runtime helper for checking keyCodes from config.
  * exposed as Vue.prototype._k
@@ -15,12 +35,11 @@ export function checkKeyCodes (
   eventKeyName?: string
 ): ?boolean {
   const keyCodes = config.keyCodes[key] || builtInAlias
-  if (keyCodes) {
-    if (Array.isArray(keyCodes)) {
-      return keyCodes.indexOf(eventKeyCode) === -1
-    } else {
-      return keyCodes !== eventKeyCode
-    }
+  const builtInName: string | Array<string> = keyNames[key]
+  if (builtInName && keyCodes === builtInAlias && eventKeyName) {
+    return isKeyNotMatch(builtInName, eventKeyName)
+  } else if (keyCodes) {
+    return isKeyNotMatch(keyCodes, eventKeyCode)
   } else if (eventKeyName) {
     return hyphenate(eventKeyName) !== key
   }
