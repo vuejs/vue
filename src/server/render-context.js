f322eecd6be817ff183673312f435c9ce18ccf67
Evan You <yyx990803@gmail.com>
2018-03-12 09:30:29 -0400

    Update render-context.js

--- src/server/render-context.js
+++ src/server/render-context.js
@@ -79,7 +79,7 @@ export class RenderContext {
           this.renderNode(children[rendered], false, this)
         } else {
           this.renderStates.pop()
-          if (lastState.endTag) {
+          if (lastState.type === 'Element') {
             this.write(lastState.endTag, this.next)
           } else {
             this.next()
