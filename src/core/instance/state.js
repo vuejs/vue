6c8f4a983b0ae924cbc2a63395fa9d2bd2716464
HcySunYang <HcySunYang@outlook.com>
2018-03-22 12:16:15 +0800

    refactor: remove unnecessary checks

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -111,9 +111,7 @@ function initProps (vm: Component, propsOptions: Object) {
 
 function initData (vm: Component) {
   let data = vm.$options.data
-  data = vm._data = typeof data === 'function'
-    ? getData(data, vm)
-    : data || {}
+  data = vm._data = getData(data, vm)
   if (!isPlainObject(data)) {
     data = {}
     process.env.NODE_ENV !== 'production' && warn(
