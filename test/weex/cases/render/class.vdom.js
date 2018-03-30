566bcf93961d6c2e0bc0396f8a84799b2579295c
Zhenfei You <hello@imyzf.com>
2018-03-30 17:02:55 +0800

    feat(weex): support object syntax of class

---
+++ test/weex/cases/render/class.vdom.js
@@ -0,0 +1,16 @@
+({
+  type: 'div',
+  children: [{
+    type: 'div',
+    classList: ['box', 'box1']
+  }, {
+    type: 'div',
+    classList: ['box', 'box2']
+  }, {
+    type: 'div',
+    classList: ['box', 'box3']
+  }, {
+    type: 'div',
+    classList: ['box', 'box4']
+  }]
+})
