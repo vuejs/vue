7a0ab815645ea56e0e0727b9d218c25f193fc7da
banxi1988 <banxi1988@gmail.com>
2018-05-09 17:44:01 +0800

    refactor: wraps those three var in a object to indicate that they are not local var

--- src/core/observer/scheduler.js
+++ src/core/observer/scheduler.js
@@ -16,27 +16,29 @@ const queue: Array<Watcher> = []
 const activatedChildren: Array<Component> = []
 let has: { [key: number]: ?true } = {}
 let circular: { [key: number]: number } = {}
-let waiting = false
-let flushing = false
-let index = 0
+const flushState = {
+  waiting: false,
+  flushing: false,
+  index: 0
+}
 
 /**
  * Reset the scheduler's state.
  */
 function resetSchedulerState () {
-  index = queue.length = activatedChildren.length = 0
+  flushState.index = queue.length = activatedChildren.length = 0
   has = {}
   if (process.env.NODE_ENV !== 'production') {
     circular = {}
   }
-  waiting = flushing = false
+  flushState.waiting = flushState.flushing = false
 }
 
 /**
  * Flush both queues and run the watchers.
  */
 function flushSchedulerQueue () {
-  flushing = true
+  flushState.flushing = true
   let watcher, id
 
   // Sort queue before flush.
@@ -51,8 +53,8 @@ function flushSchedulerQueue () {
 
   // do not cache length because more watchers might be pushed
   // as we run existing watchers
-  for (index = 0; index < queue.length; index++) {
-    watcher = queue[index]
+  for (flushState.index = 0; flushState.index < queue.length; flushState.index++) {
+    watcher = queue[flushState.index]
     if (watcher.before) {
       watcher.before()
     }
@@ -131,20 +133,20 @@ export function queueWatcher (watcher: Watcher) {
   const id = watcher.id
   if (has[id] == null) {
     has[id] = true
-    if (!flushing) {
+    if (!flushState.flushing) {
       queue.push(watcher)
     } else {
       // if already flushing, splice the watcher based on its id
       // if already past its id, it will be run next immediately.
       let i = queue.length - 1
-      while (i > index && queue[i].id > watcher.id) {
+      while (i > flushState.index && queue[i].id > watcher.id) {
         i--
       }
       queue.splice(i + 1, 0, watcher)
     }
     // queue the flush
-    if (!waiting) {
-      waiting = true
+    if (!flushState.waiting) {
+      flushState.waiting = true
       nextTick(flushSchedulerQueue)
     }
   }
