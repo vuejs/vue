78f81484af4e33365744e37925501344b1ff309a
Haoqun Jiang <haoqunjiang@gmail.com>
2018-06-20 14:27:24 +0800

    fix(lifecycle): updated should not be called after component being destroyed, fix #8076

--- src/core/observer/scheduler.js
+++ src/core/observer/scheduler.js
@@ -98,7 +98,7 @@ function callUpdatedHooks (queue) {
   while (i--) {
     const watcher = queue[i]
     const vm = watcher.vm
-    if (vm._watcher === watcher && vm._isMounted) {
+    if (vm._watcher === watcher && vm._isMounted && !vm._isDestroyed) {
       callHook(vm, 'updated')
     }
   }
