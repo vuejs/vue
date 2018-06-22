01833c5679a8c06dfc828ce88df1ceaf907bd180
xiongzixiao <xiong_zixiao@163.com>
2018-06-22 18:01:03 +0800

    use feature detection instead of browser sniffing

--- src/core/util/env.js
+++ src/core/util/env.js
@@ -8,12 +8,12 @@ export const inBrowser = typeof window !== 'undefined'
 export const inWeex = typeof WXEnvironment !== 'undefined' && !!WXEnvironment.platform
 export const weexPlatform = inWeex && WXEnvironment.platform.toLowerCase()
 export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
-export const isIE = UA && /msie|trident/.test(UA)
-export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
+export const isIE = inBrowser && document.documentMode
+export const isIE9 = inBrowser && document.documentMode && document.documentMode === 9
 export const isEdge = UA && UA.indexOf('edge/') > 0
 export const isAndroid = (UA && UA.indexOf('android') > 0) || (weexPlatform === 'android')
 export const isIOS = (UA && /iphone|ipad|ipod|ios/.test(UA)) || (weexPlatform === 'ios')
-export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge
+export const isChrome = inBrowser && window.chrome
 
 // Firefox has a "watch" function on Object.prototype...
 export const nativeWatch = ({}).watch
