4df1f1df19f48f34ee394890e352ee78c95183f9
Daniel Duton <daniel@duton.lu>
2018-07-02 15:09:19 +0200

    fix(v-model): transformModel should check if prop is defined and fallback to attrs if needed (issue #8430)

--- src/core/vdom/create-component.js
+++ src/core/vdom/create-component.js
@@ -247,7 +247,12 @@ function installComponentHooks (data: VNodeData) {
 function transformModel (options, data: any) {
   const prop = (options.model && options.model.prop) || 'value'
   const event = (options.model && options.model.event) || 'input'
-  ;(data.props || (data.props = {}))[prop] = data.model.value
+  // check if prop is defined, if not, attrs will be used
+  if (options.props && options.props[prop]) {
+    ;(data.props || (data.props = {}))[prop] = data.model.value
+  } else {
+    ;(data.attrs || (data.attrs = {}))[prop] = data.model.value
+  }
   const on = data.on || (data.on = {})
   if (isDef(on[event])) {
     on[event] = [data.model.callback].concat(on[event])
