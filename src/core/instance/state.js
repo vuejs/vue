d1bf5dab933ffa85f013201a65137548259f55c2
Aleksei Tirman <aleksei.tirman@gmail.com>
2018-10-21 00:50:15 +0300

    feat(state.js): check if method is not a function in the component definition
    warn about method is not a function in the development mode, pass noop if it is not a function
    
    fix #8017

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -259,9 +259,9 @@ function initMethods (vm: Component, methods: Object) {
   const props = vm.$options.props
   for (const key in methods) {
     if (process.env.NODE_ENV !== 'production') {
-      if (methods[key] == null) {
+      if (typeof methods[key] !== 'function') {
         warn(
-          `Method "${key}" has an undefined value in the component definition. ` +
+          `Method "${key}" has type "${typeof methods[key]}" in the component definition. ` +
           `Did you reference the function correctly?`,
           vm
         )
@@ -279,7 +279,7 @@ function initMethods (vm: Component, methods: Object) {
         )
       }
     }
-    vm[key] = methods[key] == null ? noop : bind(methods[key], vm)
+    vm[key] = typeof methods[key] !== 'function' ? noop : bind(methods[key], vm)
   }
 }
 
