390ff8ede828f75795d1a880072159e3fc549e81
Clark Du <clark.duxin@gmail.com>
2018-10-23 21:26:20 +0100

    refactor: add missing else in genNode (#8600)

--- src/compiler/codegen/index.js
+++ src/compiler/codegen/index.js
@@ -435,7 +435,7 @@ function needsNormalization (el: ASTElement): boolean {
 function genNode (node: ASTNode, state: CodegenState): string {
   if (node.type === 1) {
     return genElement(node, state)
-  } if (node.type === 3 && node.isComment) {
+  } else if (node.type === 3 && node.isComment) {
     return genComment(node)
   } else {
     return genText(node)
