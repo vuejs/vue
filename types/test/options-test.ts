823a97fb25bdd7abdf0341d2ce249a0f53ce5e3f
Kael <kaelwd@gmail.com>
2018-10-25 04:05:15 +1100

    types: add refInFor to VNodeData (#8229)

--- types/test/options-test.ts
+++ types/test/options-test.ts
@@ -176,7 +176,8 @@ Vue.component('component', {
         fontSize: '14px'
       },
       key: 'myKey',
-      ref: 'myRef'
+      ref: 'myRef',
+      refInFor: true
     }, [
       createElement(),
       createElement("div", "message"),
