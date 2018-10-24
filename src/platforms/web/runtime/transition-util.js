e5b7db29f1052bdfa54db3070f339c5ebac12dc1
Phablulo Joel <pjs2@cin.ufpe.br>
2018-10-24 14:58:25 -0300

    fix(transition): handle local-formatted floats in toMs function.  (#8495)
    fix #4894

--- src/platforms/web/runtime/transition-util.js
+++ src/platforms/web/runtime/transition-util.js
@@ -181,6 +181,10 @@ function getTimeout (delays: Array<string>, durations: Array<string>): number {
   }))
 }
 
+// Old versions of Chromium (below 61.0.3163.100) formats floating pointer numbers
+// in a locale-dependent way, using a comma instead of a dot.
+// If comma is not replaced with a dot, the input will be rounded down (i.e. acting
+// as a floor function) causing unexpected behaviors
 function toMs (s: string): number {
-  return Number(s.slice(0, -1)) * 1000
+  return Number(s.slice(0, -1).replace(',', '.')) * 1000
 }
