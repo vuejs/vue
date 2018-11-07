99765c77b2371143feb9e1b220eda0a912d8733e
Guillaume Chau <alphadelta.fg@gmail.com>
2018-11-06 22:53:53 -0800

    fix: ssrPrefetch not an array

--- src/server/render.js
+++ src/server/render.js
@@ -50,8 +50,9 @@ const normalizeRender = vm => {
 }
 
 function waitForSsrPrefetch (vm, resolve, reject) {
-  const handlers = vm.$options.ssrPrefetch
+  let handlers = vm.$options.ssrPrefetch
   if (isDef(handlers)) {
+    if (!Array.isArray(handlers)) handlers = [handlers]
     try {
       const promises = []
       for (let i = 0, j = handlers.length; i < j; i++) {
