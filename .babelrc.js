1ddb29507c4132fa69f1a5e24865327808be9450
LiYi <1209838128@qq.com>
2018-11-30 14:02:34 +0800

    33

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,7 @@ const babelPresetFlowVue = {
   plugins: [
     require('@babel/plugin-proposal-class-properties'),
     // require('@babel/plugin-syntax-flow'), // not needed, included in transform-flow-strip-types
-      require('@babel/plugin-transform-flow-strip-types')
+         require('@babel/plugin-transform-flow-strip-types')
   ]
 }
 
