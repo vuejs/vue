56f4d64dae79f5eb2ddd7b4af38ed30dd85d2e8b
Matteo Fogli <matteo@modo.md>
2018-12-10 00:06:04 +0100

    fix(lifecycle): beforeUpdated should not be called if component is destroyed

--- test/unit/features/options/lifecycle.spec.js
+++ test/unit/features/options/lifecycle.spec.js
@@ -152,6 +152,43 @@ describe('Options lifecycle hooks', () => {
         expect(vm.$el.textContent).toBe('bar!')
       }).then(done)
     })
+
+    // #8076
+    it('should not be called after destroy', done => {
+      const beforeUpdated = jasmine.createSpy('beforeUpdated')
+      const destroyed = jasmine.createSpy('destroyed')
+
+      Vue.component('todo', {
+        template: '<div>{{todo.done}}</div>',
+        props: ['todo'],
+        destroyed,
+        beforeUpdated
+      })
+
+      const vm = new Vue({
+        template: `
+          <div>
+            <todo v-for="t in pendingTodos" :todo="t" :key="t.id"></todo>
+          </div>
+        `,
+        data () {
+          return {
+            todos: [{ id: 1, done: false }]
+          }
+        },
+        computed: {
+          pendingTodos () {
+            return this.todos.filter(t => !t.done)
+          }
+        }
+      }).$mount()
+
+      vm.todos[0].done = true
+      waitForUpdate(() => {
+        expect(destroyed).toHaveBeenCalled()
+        expect(beforeUpdated).not.toHaveBeenCalled()
+      }).then(done)
+    })
   })
 
   describe('updated', () => {
