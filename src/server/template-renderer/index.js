d15cc662da8de47433029258316511f555f9ab27
Clark Du <clark.duxin@gmail.com>
2018-12-12 15:15:05 +0000

    refactor: tiny cleanup changes

--- src/server/template-renderer/index.js
+++ src/server/template-renderer/index.js
@@ -207,7 +207,7 @@ export default class TemplateRenderer {
     if (this.clientManifest) {
       const initial = this.preloadFiles.filter(({ file }) => isJS(file))
       const async = (this.getUsedAsyncFiles(context) || []).filter(({ file }) => isJS(file))
-      const needed = [initial[0]].concat(async || [], initial.slice(1))
+      const needed = [initial[0]].concat(async, initial.slice(1))
       return needed.map(({ file }) => {
         return `<script src="${this.publicPath}${file}" defer></script>`
       }).join('')
