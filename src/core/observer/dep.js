6de09d74a1fc3017d0306febba63645f9773cea8
Nick Hall <iamssb@gmail.com>
2018-12-13 01:41:36 +0900

    chore: minor comment cleanup (#9191)

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -49,9 +49,9 @@ export default class Dep {
   }
 }
 
-// the current target watcher being evaluated.
-// this is globally unique because there could be only one
-// watcher being evaluated at any time.
+// The current target watcher being evaluated.
+// This is globally unique because only one watcher
+// can be evaluated at a time.
 Dep.target = null
 const targetStack = []
 
