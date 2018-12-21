a00efdd2426d39b0990a4c04c0a775164c30442d
Evan You <yyx990803@gmail.com>
2018-12-21 13:01:01 -0500

    Update util.js

--- src/shared/util.js
+++ src/shared/util.js
@@ -77,7 +77,7 @@ export function isValidArrayIndex (val: any): boolean {
 export function toString (val: any): string {
   return val == null
     ? ''
-    : typeof val === 'object' && (!isPlainObject(val) || val.toString === _toString)
+    : isPlainObject(val) && val.toString === _toString
       ? JSON.stringify(val, null, 2)
       : String(val)
 }
