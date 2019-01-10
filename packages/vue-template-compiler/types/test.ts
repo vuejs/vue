142cf6c4893c36a20e9e4102f588103c70023b46
Evan You <yyx990803@gmail.com>
2019-01-10 18:19:23 -0500

    types: update typing for vue-template-compiler

--- packages/vue-template-compiler/types/test.ts
+++ packages/vue-template-compiler/types/test.ts
@@ -9,6 +9,7 @@ import {
 
 // check compile options
 const compiled = compile("<div>hi</div>", {
+  outputSourceRange: true,
   preserveWhitespace: false,
   whitespace: 'condense',
   modules: [
@@ -34,6 +35,30 @@ const compiled = compile("<div>hi</div>", {
 // can be passed to function constructor
 new Function(compiled.render);
 compiled.staticRenderFns.map(fn => new Function(fn));
+
+// with outputSourceRange: true
+// errors should be objects with range
+compiled.errors.forEach(e => {
+  console.log(e.msg)
+})
+
+// without option or without outputSourceRange: true, should be strings
+const { errors } = compile(`foo`)
+errors.forEach(e => {
+  console.log(e.length)
+})
+
+const { errors: errors2 } = compile(`foo`, {})
+errors2.forEach(e => {
+  console.log(e.length)
+})
+
+const { errors: errors3 } = compile(`foo`, {
+  outputSourceRange: false
+})
+errors3.forEach(e => {
+  console.log(e.length)
+})
 
 const compiledFns = compileToFunctions("<div>hi</div>");
 
