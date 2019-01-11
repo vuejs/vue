7b7b34f4c698495c3291d28bb1b3733412db9696
Evan You <yyx990803@gmail.com>
2019-01-11 14:54:10 -0500

    fix(ssr): should not render invalid numeric style values
    Reverts part of 7d9cfebe - browsers only auto append units to
    numbers when in quirksmode, so in standard mode, numbers set to
    style properties that require units are invalid and should not
    be rendered.

--- src/platforms/web/server/modules/style.js
+++ src/platforms/web/server/modules/style.js
@@ -21,18 +21,11 @@ export function genStyle (style: Object): string {
 }
 
 function normalizeValue(key: string, value: any): string {
-  if (typeof value === 'string') {
+  if (
+    typeof value === 'string' ||
+    (typeof value === 'number' && noUnitNumericStyleProps[key])
+  ) {
     return `${key}:${value};`
-  } else if (typeof value === 'number') {
-    // Handle numeric values.
-    // Turns out all evergreen browsers plus IE11 already support setting plain
-    // numbers on the style object and will automatically convert it to px if
-    // applicable, so we should support that on the server too.
-    if (noUnitNumericStyleProps[key]) {
-      return `${key}:${value};`
-    } else {
-      return `${key}:${value}px;`
-    }
   } else {
     // invalid values
     return ``
