8bc1955258e303fc2cce822a4396b76ab19e7ab8
Evan You <yyx990803@gmail.com>
2019-01-11 11:07:40 -0500

    fix(ssr): properly handle invalid and numeric style properties
    Ignores values that are not string or numbers, and append px as default
    unit to appropriate properties.
    
    There will still be certain cases where the user simply provides an
    invalid string value to a property which will be too costly to detect
    (it's possible by using the `cssstyle` package, but very heavy). But
    in such cases the user would already notice the style is not working
    on the client, so it's not really worth it for the perf implications.
    
    fix #9231

--- src/platforms/web/server/modules/style.js
+++ src/platforms/web/server/modules/style.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import { escape } from '../util'
+import { escape, noUnitNumericStyleProps } from '../util'
 import { hyphenate } from 'shared/util'
 import { getStyle } from 'web/util/style'
 
@@ -11,13 +11,32 @@ export function genStyle (style: Object): string {
     const hyphenatedKey = hyphenate(key)
     if (Array.isArray(value)) {
       for (let i = 0, len = value.length; i < len; i++) {
-        styleText += `${hyphenatedKey}:${value[i]};`
+        styleText += normalizeValue(hyphenatedKey, value[i])
       }
     } else {
-      styleText += `${hyphenatedKey}:${value};`
+      styleText += normalizeValue(hyphenatedKey, value)
     }
   }
   return styleText
+}
+
+function normalizeValue(key: string, value: any): string {
+  if (typeof value === 'string') {
+    return `${key}:${value};`
+  } else if (typeof value === 'number') {
+    // Handle numeric values.
+    // Turns out all evergreen browsers plus IE11 already support setting plain
+    // numbers on the style object and will automatically convert it to px if
+    // applicable, so we should support that on the server too.
+    if (noUnitNumericStyleProps[key]) {
+      return `${key}:${value};`
+    } else {
+      return `${key}:${value}px;`
+    }
+  } else {
+    // invalid values
+    return ``
+  }
 }
 
 export default function renderStyle (vnode: VNodeWithData): ?string {
