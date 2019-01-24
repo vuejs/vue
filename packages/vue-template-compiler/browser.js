fc3f8c7a91cd2cdc1df4a899891492985fa29430
Kermit <kermitlx@outlook.com>
2019-01-24 12:57:30 +0800

    fix(genInlineTemplate): handle error when children is empty(#9361)

--- packages/vue-template-compiler/browser.js
+++ packages/vue-template-compiler/browser.js
@@ -4338,15 +4338,13 @@
 
   function genInlineTemplate (el, state) {
     var ast = el.children[0];
-    if (process.env.NODE_ENV !== 'production' && (
-      el.children.length !== 1 || ast.type !== 1
-    )) {
+    if (el.children.length !== 1 || ast.type !== 1) {
       state.warn(
         'Inline-template components must have exactly one child element.',
         { start: el.start }
       );
     }
-    if (ast.type === 1) {
+    if (ast && ast.type === 1) {
       var inlineRenderFns = generate(ast, state.options);
       return ("inlineTemplate:{render:function(){" + (inlineRenderFns.render) + "},staticRenderFns:[" + (inlineRenderFns.staticRenderFns.map(function (code) { return ("function(){" + code + "}"); }).join(',')) + "]}")
     }
