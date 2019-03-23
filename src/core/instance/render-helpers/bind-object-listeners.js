aa36dc802e7fe9f8a31a20da3700ea53efbfc154
shasharoman <shasharoman@gmail.com>
2019-03-23 21:32:16 +0800

    feat(#9718): v-on support listeners array

--- src/core/instance/render-helpers/bind-object-listeners.js
+++ src/core/instance/render-helpers/bind-object-listeners.js
@@ -4,9 +4,19 @@ import { warn, extend, isPlainObject } from 'core/util/index'
 
 export function bindObjectListeners (data: any, value: any): VNodeData {
   if (value) {
-    if (!isPlainObject(value)) {
+    const isArrayWithPlainObject = Array.isArray(value) && !value.some(item => !isPlainObject(item));
+    if (isArrayWithPlainObject) {
+      value = value.reduce((prev, item) => {
+        for (const key in item) {
+           prev[key] = prev[key] ? [].concat(prev[key], item[key]) : item[key]
+        }
+        return prev
+      }, {})
+    }
+
+    if (!isPlainObject(value) || (Array.isArray(value) && !isArrayWithPlainObject)) {
       process.env.NODE_ENV !== 'production' && warn(
-        'v-on without argument expects an Object value',
+        'v-on without argument expects an Object value or Array containing Object',
         this
       )
     } else {
