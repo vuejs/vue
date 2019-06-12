a8d61404cc17c30c0f907143e1cd64a01b76b20e
liximomo <liximomo@gmail.com>
2019-06-12 21:12:53 +0800

    fix: fix memory leak of watcher created in initComputed
    after vue instance get destroyed, the watcher created in initComputed can still collect deps, which
    cause the watcher can't be GC.

--- src/core/instance/state.js
+++ src/core/instance/state.js
@@ -242,7 +242,7 @@ function createComputedGetter (key) {
   return function computedGetter () {
     const watcher = this._computedWatchers && this._computedWatchers[key]
     if (watcher) {
-      if (watcher.dirty) {
+      if (watcher.active && watcher.dirty) {
         watcher.evaluate()
       }
       if (Dep.target) {
