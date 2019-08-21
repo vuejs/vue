dcdb8869a39c4ee9cf7d08102a4fd9e3aec454cf
Hassan Nteifeh <hassan.nteifeh@outlook.com>
2019-08-21 18:31:04 +0200

    feat(core): allow users to create a Vue-instance without the "new" keyword

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -3,13 +3,12 @@ import { stateMixin } from './state'
 import { renderMixin } from './render'
 import { eventsMixin } from './events'
 import { lifecycleMixin } from './lifecycle'
-import { warn } from '../util/index'
 
 function Vue (options) {
   if (process.env.NODE_ENV !== 'production' &&
     !(this instanceof Vue)
   ) {
-    warn('Vue is a constructor and should be called with the `new` keyword')
+   return new Vue(options)
   }
   this._init(options)
 }
