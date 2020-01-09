7f9b96ed75d4546fd42c546036fc8d1737432754
Ahuing <guohuihot@163.com>
2020-01-09 18:04:21 +0800

    Create jsx-render.js
    component jsx render

---
+++ src/core/components/jsx-render.js
@@ -0,0 +1,16 @@
+/* @flow */
+
+export default {
+  name: 'jsx-render',
+  props: {
+    renderContent: {
+      type: [Object, String, Array, Function],
+      default() {
+        return {}
+      }
+    }
+  },
+  render(h) {
+    return typeof this.renderContent == 'function' ? this.renderContent() : this.renderContent
+  }
+}
