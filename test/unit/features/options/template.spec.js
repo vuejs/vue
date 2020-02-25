a3933bae6f6230109ab4f4437bdf89c2418a2309
Tyler Schroeder <tschroeder@surgeforward.com>
2020-02-25 09:00:51 -0800

    fix(error-detector.js): fix incorrect unary warning on object functions
    Update error-detector to look for `.` prefix, indicating function is associated with an object
    
    fix #11090

--- test/unit/features/options/template.spec.js
+++ test/unit/features/options/template.spec.js
@@ -1,91 +1,103 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-describe('Options template', () => {
-  let el
+describe("Options template", () => {
+  let el;
   beforeEach(() => {
-    el = document.createElement('script')
-    el.type = 'x-template'
-    el.id = 'app'
-    el.innerHTML = '<p>{{message}}</p>'
-    document.body.appendChild(el)
-  })
+    el = document.createElement("script");
+    el.type = "x-template";
+    el.id = "app";
+    el.innerHTML = "<p>{{message}}</p>";
+    document.body.appendChild(el);
+  });
 
   afterEach(() => {
-    document.body.removeChild(el)
-  })
+    document.body.removeChild(el);
+  });
 
-  it('basic usage', () => {
+  it("basic usage", () => {
     const vm = new Vue({
-      template: '<div>{{message}}</div>',
-      data: { message: 'hello world' }
-    }).$mount()
-    expect(vm.$el.tagName).toBe('DIV')
-    expect(vm.$el.textContent).toBe(vm.message)
-  })
+      template: "<div>{{message}}</div>",
+      data: { message: "hello world" }
+    }).$mount();
+    expect(vm.$el.tagName).toBe("DIV");
+    expect(vm.$el.textContent).toBe(vm.message);
+  });
 
-  it('id reference', () => {
+  it("id reference", () => {
     const vm = new Vue({
-      template: '#app',
-      data: { message: 'hello world' }
-    }).$mount()
-    expect(vm.$el.tagName).toBe('P')
-    expect(vm.$el.textContent).toBe(vm.message)
-  })
+      template: "#app",
+      data: { message: "hello world" }
+    }).$mount();
+    expect(vm.$el.tagName).toBe("P");
+    expect(vm.$el.textContent).toBe(vm.message);
+  });
 
-  it('DOM element', () => {
-    const elm = document.createElement('p')
-    elm.innerHTML = '<p>{{message}}</p>'
+  it("DOM element", () => {
+    const elm = document.createElement("p");
+    elm.innerHTML = "<p>{{message}}</p>";
     const vm = new Vue({
       template: elm,
-      data: { message: 'hello world' }
-    }).$mount()
-    expect(vm.$el.tagName).toBe('P')
-    expect(vm.$el.textContent).toBe(vm.message)
-  })
+      data: { message: "hello world" }
+    }).$mount();
+    expect(vm.$el.tagName).toBe("P");
+    expect(vm.$el.textContent).toBe(vm.message);
+  });
 
-  it('invalid template', () => {
+  it("invalid template", () => {
     new Vue({
       template: Vue,
-      data: { message: 'hello world' }
-    }).$mount()
-    expect('invalid template option').toHaveBeenWarned()
-  })
+      data: { message: "hello world" }
+    }).$mount();
+    expect("invalid template option").toHaveBeenWarned();
+  });
 
-  it('warn error in generated function', () => {
+  it("warn error in generated function", () => {
     new Vue({
-      template: '<div v-if="!@"><span>{{ a"" }}</span><span>{{ do + 1 }}</span></div>'
-    }).$mount()
-    expect('Error compiling template').toHaveBeenWarned()
-    expect('Raw expression: v-if="!@"').toHaveBeenWarned()
-    expect('Raw expression: {{ a"" }}').toHaveBeenWarned()
-    expect('avoid using JavaScript keyword as property name: "do"').toHaveBeenWarned()
-  })
+      template:
+        '<div v-if="!@"><span>{{ a"" }}</span><span>{{ do + 1 }}</span></div>'
+    }).$mount();
+    expect("Error compiling template").toHaveBeenWarned();
+    expect('Raw expression: v-if="!@"').toHaveBeenWarned();
+    expect('Raw expression: {{ a"" }}').toHaveBeenWarned();
+    expect(
+      'avoid using JavaScript keyword as property name: "do"'
+    ).toHaveBeenWarned();
+  });
 
-  it('should not warn $ prefixed keywords', () => {
+  it("should not warn $ prefixed keywords", () => {
     new Vue({
       template: `<div @click="$delete(foo, 'bar')"></div>`
-    }).$mount()
-    expect('avoid using JavaScript keyword as property name').not.toHaveBeenWarned()
-  })
+    }).$mount();
+    expect(
+      "avoid using JavaScript keyword as property name"
+    ).not.toHaveBeenWarned();
+  });
 
-  it('warn error in generated function (v-for)', () => {
+  it("warn error in generated function (v-for)", () => {
     new Vue({
       template: '<div><div v-for="(1, 2) in a----"></div></div>'
-    }).$mount()
-    expect('Error compiling template').toHaveBeenWarned()
-    expect('invalid v-for alias "1"').toHaveBeenWarned()
-    expect('invalid v-for iterator "2"').toHaveBeenWarned()
-    expect('Raw expression: v-for="(1, 2) in a----"').toHaveBeenWarned()
-  })
+    }).$mount();
+    expect("Error compiling template").toHaveBeenWarned();
+    expect('invalid v-for alias "1"').toHaveBeenWarned();
+    expect('invalid v-for iterator "2"').toHaveBeenWarned();
+    expect('Raw expression: v-for="(1, 2) in a----"').toHaveBeenWarned();
+  });
 
-  it('warn error in generated function (v-on)', () => {
+  it("warn error in generated function (v-on)", () => {
     new Vue({
       template: `<div @click="delete('Delete')"></div>`,
-      methods: { delete: function () {} }
-    }).$mount()
-    expect('Error compiling template').toHaveBeenWarned()
+      methods: { delete: function() {} }
+    }).$mount();
+    expect("Error compiling template").toHaveBeenWarned();
     expect(
       `avoid using JavaScript unary operator as property name: "delete()" in expression @click="delete('Delete')"`
-    ).toHaveBeenWarned()
-  })
-})
+    ).toHaveBeenWarned();
+  });
+  it("don't throw error when using reserved word which is prefixed by object", () => {
+    Vue.prototype.$http = { delete: () => {} };
+    new Vue({
+      template: `<div @click="$http.delete('/endpoint')"></div>`
+    }).$mount();
+    expect("Error compiling template").not.toHaveBeenWarned();
+  });
+});
