127a4c17eb8cd044dad31a0af70555b5477266b2
卢欣怡 <Chinansjt@163.com>
2020-04-22 14:50:30 +0800

    测试提交

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -8,6 +8,7 @@ import { createCompilerCreator } from './create-compiler'
 // `createCompilerCreator` allows creating compilers that use alternative
 // parser/optimizer/codegen, e.g the SSR optimizing compiler.
 // Here we just export a default compiler using the default parts.
+// 测试提交
 export const createCompiler = createCompilerCreator(function baseCompile (
   template: string,
   options: CompilerOptions
