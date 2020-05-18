fc10ee23677703134dcc6b8619f8771503b5a0ff
Raz Luvaton <rluvaton@gmail.com>
2020-05-18 19:50:53 +0300

    Fix isNative function to be more reliable
    Fix #11400

--- src/core/util/env.js
+++ src/core/util/env.js
@@ -55,7 +55,7 @@ export const devtools = inBrowser && window.__VUE_DEVTOOLS_GLOBAL_HOOK__
 
 /* istanbul ignore next */
 export function isNative (Ctor: any): boolean {
-  return typeof Ctor === 'function' && /native code/.test(Ctor.toString())
+  return typeof Ctor === 'function' && Ctor.toString() === 'function() { [native code] }';
 }
 
 export const hasSymbol =
