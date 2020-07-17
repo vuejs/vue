afa1705440a84101dd7254cd768468011f8f01b2
Callum Chaney <35102876+CallumChaney@users.noreply.github.com>
2020-07-17 17:29:46 +0100

    Clean up for PR

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -62,7 +62,7 @@ export class Observer {
    * value type is Object.
    */
   walk (obj: Object) {
-    let keys = Object.keys(obj)
+    const keys = Object.keys(obj)
     for (let i = 0; i < keys.length; i++) {
       if(keys[i] == ''){
         warn(`Objects should not contain empty keys cannot bind to value`)
