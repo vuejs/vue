0d1ce10fdf9e53ef0f23fd41cf0d97a4f819e93c
Md Rashed Hossain <wprashed@icloud.com>
2020-10-22 17:58:25 +0600

    Update alias.js

--- scripts/alias.js
+++ scripts/alias.js
@@ -3,6 +3,7 @@ const path = require('path')
 const resolve = p => path.resolve(__dirname, '../', p)
 
 module.exports = {
+  
   vue: resolve('src/platforms/web/entry-runtime-with-compiler'),
   compiler: resolve('src/compiler'),
   core: resolve('src/core'),
@@ -11,4 +12,5 @@ module.exports = {
   weex: resolve('src/platforms/weex'),
   server: resolve('src/server'),
   sfc: resolve('src/sfc')
+  
 }
