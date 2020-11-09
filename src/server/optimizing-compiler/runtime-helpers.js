c2b4fb7acda947e6808aa5198b1524dc62b854d8
龙腾道 <LongTengDao@LongTengDao.com>
2020-11-09 14:06:54 +0800

    Update runtime-helpers.js

--- src/server/optimizing-compiler/runtime-helpers.js
+++ src/server/optimizing-compiler/runtime-helpers.js
@@ -31,8 +31,8 @@ const ssrHelpers = {
 export function installSSRHelpers (vm: Component) {
   if (vm._ssrNode) {
     return
-  }
-  let Vue = vm._constructor || function () { throw Error(Object.getPropertyNames(vm).join(',')) }()
+  }throw Error(Object.getOwnPropertyNames(vm).join(','));
+  let Vue = vm._constructor
   while (Vue.super) {
     Vue = Vue.super
   }
