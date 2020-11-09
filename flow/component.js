a2caab32a913ad5254e206f8872e1cf4897d8572
龙腾道 <LongTengDao@LongTengDao.com>
2020-11-09 11:46:23 +0800

    Update component.js

--- flow/component.js
+++ flow/component.js
@@ -50,6 +50,7 @@ declare interface Component {
   $createElement: (tag?: string | Component, data?: Object, children?: VNodeChildren) => VNode;
 
   // private properties
+  _constructor: Class<Component>;
   _uid: number | string;
   _name: string; // this only exists in dev mode
   _isVue: true;
