35b689d307140acb7c66e098903e00aa695035d1
Sebastian Krüger <2pi_r2@gmx.de>
2021-02-19 15:15:36 +0100

    remove not needed , and ;

--- types/test/async-component-test.ts
+++ types/test/async-component-test.ts
@@ -2,51 +2,51 @@ import Vue, { AsyncComponent, Component } from "../index";
 
 const a: AsyncComponent = () => ({
   component: new Promise<Component>((res, rej) => {
-    res({ template: "" });
-  }),
-});
+    res({ template: "" })
+  })
+})
 
 const b: AsyncComponent = () => ({
   // @ts-expect-error component has to be a Promise that resolves to a component
   component: () =>
     new Promise<Component>((res, rej) => {
-      res({ template: "" });
-    }),
-});
+      res({ template: "" })
+    })
+})
 
 const c: AsyncComponent = () =>
   new Promise<Component>((res, rej) => {
     res({
-      template: "",
-    });
-  });
+      template: ""
+    })
+  })
 
 const d: AsyncComponent = () =>
   new Promise<{ default: Component }>((res, rej) => {
     res({
       default: {
-        template: "",
-      },
-    });
-  });
+        template: ""
+      }
+    })
+  })
 
 const e: AsyncComponent = () => ({
   component: new Promise<{ default: Component }>((res, rej) => {
     res({
       default: {
-        template: "",
-      },
-    });
-  }),
-});
+        template: ""
+      }
+    })
+  })
+})
 
 Vue.component("async-compponent1", () => ({
   component: new Promise<Component>((res, rej) => {
     res({
-      template: "",
-    });
-  }),
-}));
+      template: ""
+    })
+  })
+}))
 
 Vue.component(
   "async-compponent2",
@@ -54,8 +54,8 @@ Vue.component(
     new Promise<{ default: Component }>((res, rej) => {
       res({
         default: {
-          template: "",
-        },
-      });
+          template: ""
+        }
+      })
     })
-);
+)
