d0cf0752ce76fc75ae835d365b42c0872135a39b
pooya parsa <pyapar@gmail.com>
2021-03-24 11:41:28 +0100

    ensure res.length is 1
    Co-authored-by: Eduardo San Martin Morote <posva@users.noreply.github.com>

--- src/core/vdom/helpers/normalize-scoped-slots.js
+++ src/core/vdom/helpers/normalize-scoped-slots.js
@@ -61,7 +61,7 @@ function normalizeScopedSlot(normalSlots, key, fn) {
     res = res && typeof res === 'object' && !Array.isArray(res)
       ? [res] // single vnode
       : normalizeChildren(res)
-    let vnode: VNode = res && res[0]
+    let vnode: VNode = res && res.length === 1 && res[0]
     return res && (
       !vnode ||
       (vnode.isComment && !isAsyncPlaceholder(vnode)) // #9658, #10391
