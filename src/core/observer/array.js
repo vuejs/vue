e19d95aac5ffce1dc450fd3940bd8f5e154dbc2f
Souler <46154149+1512344358@users.noreply.github.com>
2021-06-15 16:12:14 +0800

    Update array.js

--- src/core/observer/array.js
+++ src/core/observer/array.js
@@ -5,9 +5,10 @@
 
 import { def } from '../util/index'
 
-const arrayProto = Array.prototype
-export const arrayMethods = Object.create(arrayProto)
+const arrayProto = Array.prototype// 复制数组构造函数的原型，Array.prototype也是一个数组
+export const arrayMethods = Object.create(arrayProto)// 创建对象，对象的__proto__指向arrayProto，所以arrayMethods的__proto__包含数组的所有方法。
 
+// 下面的数组是要进行重写的方法
 const methodsToPatch = [
   'push',
   'pop',
@@ -21,23 +22,24 @@ const methodsToPatch = [
 /**
  * Intercept mutating methods and emit events
  */
-methodsToPatch.forEach(function (method) {
+methodsToPatch.forEach(function (method) {// 遍历methodsToPatch数组，对其中的方法进行重写
   // cache original method
   const original = arrayProto[method]
-  def(arrayMethods, method, function mutator (...args) {
+  def(arrayMethods, method, function mutator (...args) {// def方法定义在lang.js文件中，是通过object.defineProperty对属性进行重新定义。
+    // 即在arrayMethods中找到我们要重写的方法，对其进行重新定义
     const result = original.apply(this, args)
     const ob = this.__ob__
     let inserted
-    switch (method) {
+    switch (method) {// 上面已经分析过，对于push，unshift会新增索引，所以需要手动observe
       case 'push':
       case 'unshift':
         inserted = args
         break
-      case 'splice':
+      case 'splice':// splice方法，如果传入了第三个参数，也会有新增索引，所以也需要手动observe
         inserted = args.slice(2)
         break
     }
-    if (inserted) ob.observeArray(inserted)
+    if (inserted) ob.observeArray(inserted)// push，unshift，splice三个方法触发后，在这里手动observe，其他方法的变更会在当前的索引上进行更新，所以不需要再执行ob.observeArray
     // notify change
     ob.dep.notify()
     return result
