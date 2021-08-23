18b5be7c19b7b246f173d7a7b3584f9444242414
hazlank <401840614@qq.com>
2021-08-23 12:13:19 +0800

    style: delete useful code

--- src/core/instance/lifecycle.js
+++ src/core/instance/lifecycle.js
@@ -7,7 +7,6 @@ import { createEmptyVNode } from '../vdom/vnode'
 import { updateComponentListeners } from './events'
 import { resolveSlots } from './render-helpers/resolve-slots'
 import { toggleObserving } from '../observer/index'
-import { pushTarget, popTarget } from '../observer/dep'
 
 import {
   warn,
@@ -335,8 +334,6 @@ export function deactivateChildComponent (vm: Component, direct?: boolean) {
 }
 
 export function callHook (vm: Component, hook: string) {
-  // #7573 disable dep collection when invoking lifecycle hooks
-  pushTarget()
   const handlers = vm.$options[hook]
   const info = `${hook} hook`
   if (handlers) {
@@ -347,5 +344,4 @@ export function callHook (vm: Component, hook: string) {
   if (vm._hasHookEvent) {
     vm.$emit('hook:' + hook)
   }
-  popTarget()
 }
