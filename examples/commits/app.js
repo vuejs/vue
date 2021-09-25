5a888dd1033946dce55dac48c9099acf052365f6
Abderrahim SOUBAI-ELIDRISI <gx.coder@gmail.com>
2021-09-25 03:18:56 +0100

    fix: editor auto-fixer

--- examples/commits/app.js
+++ examples/commits/app.js
@@ -1,55 +1,56 @@
 /* global Vue */
 
-const apiURL = "https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha=";
+const apiURL = "https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha="
 
 /**
  * Actual demo
  */
 
 new Vue({
+
   el: "#demo",
 
   data: {
-    branches: ["master", "dev"],
-    currentBranch: "master",
-    commits: null,
+    branches: ['master', 'dev'],
+    currentBranch: 'master',
+    commits: null
   },
 
   created: function () {
-    this.fetchData();
+    this.fetchData()
   },
 
   watch: {
-    currentBranch: "fetchData",
+    currentBranch: "fetchData"
   },
 
   filters: {
     truncate: function (v) {
-      const newline = v.indexOf("\n");
-      return newline > 0 ? v.slice(0, newline) : v;
+      const newline = v.indexOf('\n')
+      return newline > 0 ? v.slice(0, newline) : v
     },
     formatDate: function (v) {
-      return v.replace(/T|Z/g, " ");
-    },
+      return v.replace(/T|Z/g, ' ')
+    }
   },
 
   methods: {
     fetchData: function () {
-      const self = this;
-      if (navigator.userAgent.indexOf("PhantomJS") > -1) {
+      const self = this
+      if (navigator.userAgent.indexOf('PhantomJS') > -1) {
         // use mocks in e2e to avoid dependency on network / authentication
         setTimeout(function () {
-          self.commits = window.MOCKS[self.currentBranch];
-        }, 0);
+          self.commits = window.MOCKS[self.currentBranch]
+        }, 0)
       } else {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", apiURL + self.currentBranch);
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', apiURL + self.currentBranch)
         xhr.onload = function () {
-          self.commits = JSON.parse(xhr.responseText);
-          console.log(self.commits[0].html_url);
-        };
-        xhr.send();
+          self.commits = JSON.parse(xhr.responseText)
+          console.log(self.commits[0].html_url)
+        }
+        xhr.send()
       }
-    },
-  },
-});
+    }
+  }
+})
