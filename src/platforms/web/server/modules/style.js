d6156fe2668c54d758afa02e29506cd257d01461
Fiete Börner <fieteboerner@googlemail.com>
2021-12-05 17:31:27 +0100

    fix(vue-server-renderer): do not hyphenate or omit CSS variables in style attribute
    close #12374

--- src/platforms/web/server/modules/style.js
+++ src/platforms/web/server/modules/style.js
@@ -4,11 +4,13 @@ import { escape, noUnitNumericStyleProps } from '../util'
 import { hyphenate } from 'shared/util'
 import { getStyle } from 'web/util/style'
 
+const cssVarRE = /^--/
+
 export function genStyle (style: Object): string {
   let styleText = ''
   for (const key in style) {
     const value = style[key]
-    const hyphenatedKey = hyphenate(key)
+      const hyphenatedKey = cssVarRE.test(key) ? key :  hyphenate(key)
     if (Array.isArray(value)) {
       for (let i = 0, len = value.length; i < len; i++) {
         styleText += normalizeValue(hyphenatedKey, value[i])
@@ -23,7 +25,8 @@ export function genStyle (style: Object): string {
 function normalizeValue(key: string, value: any): string {
   if (
     typeof value === 'string' ||
-    (typeof value === 'number' && noUnitNumericStyleProps[key]) ||
+    (typeof value === 'number' && noUnitNumericStyleProps[key]) || // accept numeric values for configured css attributes
+    (typeof value === 'number' && cssVarRE.test(key)) || // accept numeric values for all CSS variables
     value === 0
   ) {
     return `${key}:${value};`
