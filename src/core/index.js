d66d871ed5619999020b4dda1f122a0f568b9af5
ylinn2362856240 <30657325+ylinn2362856240@users.noreply.github.com>
2022-01-21 10:35:55 +0800

    Update index.js
    demo

--- src/core/index.js
+++ src/core/index.js
@@ -2,7 +2,7 @@ import Vue from './instance/index'
 import { initGlobalAPI } from './global-api/index'
 import { isServerRendering } from 'core/util/env'
 import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
-
+//
 initGlobalAPI(Vue)
 
 Object.defineProperty(Vue.prototype, '$isServer', {
