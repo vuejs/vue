e2049fc886ca5b4f75071db2a5dc966a76a544d3
罗雨凤 <1443724970@qq.com>
2022-04-25 11:56:16 +0800

    yes

--- src/core/index.js
+++ src/core/index.js
@@ -4,6 +4,7 @@ import { isServerRendering } from 'core/util/env'
 import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
 
 initGlobalAPI(Vue)
+// just test
 
 Object.defineProperty(Vue.prototype, '$isServer', {
   get: isServerRendering
