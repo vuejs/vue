12150f622b3b469904240390eaf4ccdd49eedbbb
Evan You <yyx990803@gmail.com>
2022-05-27 17:30:12 +0800

    chore: format typescript dir

--- typescript/options.d.ts
+++ typescript/options.d.ts
@@ -1,102 +1,112 @@
-import VNode from "../src/core/vdom/vnode";
-import { Component } from "./component";
+import VNode from '../src/core/vdom/vnode'
+import { Component } from './component'
 
 declare type InternalComponentOptions = {
-  _isComponent: true;
-  parent: Component;
-  _parentVnode: VNode;
-  render?: Function;
-  staticRenderFns?: Array<Function>;
-};
+  _isComponent: true
+  parent: Component
+  _parentVnode: VNode
+  render?: Function
+  staticRenderFns?: Array<Function>
+}
 
-type InjectKey = string | Symbol;
+type InjectKey = string | Symbol
+
+declare interface SetupContext {
+  attrs: Record<string, any>
+  slots: Record<string, (() => VNode[]) | undefined>
+  emit: (event: string, ...args: any[]) => any
+  expose: (exposed?: Record<string, any>) => void
+}
 
 declare type ComponentOptions = {
+  // v3
+  setup(props: Record<string, any>, ctx: SetupContext): any
+
   [key: string]: any
 
-  componentId?: string;
+  componentId?: string
 
   // data
-  data: object | Function | void;
-  props?: { [key: string]: PropOptions };
-  propsData?: object;
+  data: object | Function | void
+  props?: { [key: string]: PropOptions }
+  propsData?: object
   computed?: {
     [key: string]:
       | Function
       | {
-          get?: Function;
-          set?: Function;
-          cache?: boolean;
-        };
-  };
-  methods?: { [key: string]: Function };
-  watch?: { [key: string]: Function | string };
+          get?: Function
+          set?: Function
+          cache?: boolean
+        }
+  }
+  methods?: { [key: string]: Function }
+  watch?: { [key: string]: Function | string }
 
   // DOM
-  el?: string | Element;
-  template?: string;
-  render: (h: () => VNode) => VNode;
-  renderError?: (h: () => VNode, err: Error) => VNode;
-  staticRenderFns?: Array<() => VNode>;
+  el?: string | Element
+  template?: string
+  render: (h: () => VNode) => VNode
+  renderError?: (h: () => VNode, err: Error) => VNode
+  staticRenderFns?: Array<() => VNode>
 
   // lifecycle
-  beforeCreate?: Function;
-  created?: Function;
-  beforeMount?: Function;
-  mounted?: Function;
-  beforeUpdate?: Function;
-  updated?: Function;
-  activated?: Function;
-  deactivated?: Function;
-  beforeDestroy?: Function;
-  destroyed?: Function;
-  errorCaptured?: () => boolean | void;
-  serverPrefetch?: Function;
+  beforeCreate?: Function
+  created?: Function
+  beforeMount?: Function
+  mounted?: Function
+  beforeUpdate?: Function
+  updated?: Function
+  activated?: Function
+  deactivated?: Function
+  beforeDestroy?: Function
+  destroyed?: Function
+  errorCaptured?: () => boolean | void
+  serverPrefetch?: Function
 
   // assets
-  directives?: { [key: string]: object };
-  components?: { [key: string]: Component };
-  transitions?: { [key: string]: object };
-  filters?: { [key: string]: Function };
+  directives?: { [key: string]: object }
+  components?: { [key: string]: Component }
+  transitions?: { [key: string]: object }
+  filters?: { [key: string]: Function }
 
   // context
-  provide?: Record<string | symbol, any> | (() => Record<string | symbol, any>);
+  provide?: Record<string | symbol, any> | (() => Record<string | symbol, any>)
   inject?:
     | { [key: string]: InjectKey | { from?: InjectKey; default?: any } }
-    | Array<string>;
+    | Array<string>
 
   // component v-model customization
   model?: {
-    prop?: string;
-    event?: string;
-  };
+    prop?: string
+    event?: string
+  }
 
   // misc
-  parent?: Component;
-  mixins?: Array<object>;
-  name?: string;
-  extends?: Component | object;
-  delimiters?: [string, string];
-  comments?: boolean;
-  inheritAttrs?: boolean;
+  parent?: Component
+  mixins?: Array<object>
+  name?: string
+  extends?: Component | object
+  delimiters?: [string, string]
+  comments?: boolean
+  inheritAttrs?: boolean
 
   // Class API
   abstract?: any
 
   // private
-  _isComponent?: true;
-  _propKeys?: Array<string>;
-  _parentVnode?: VNode;
-  _parentListeners?: object | null;
-  _renderChildren?: Array<VNode> | null;
-  _componentTag: string | null;
-  _scopeId: string | null;
-  _base: Component;
-};
+  _isComponent?: true
+  _propKeys?: Array<string>
+  _parentVnode?: VNode
+  _parentListeners?: object | null
+  _renderChildren?: Array<VNode> | null
+  _componentTag: string | null
+  _scopeId: string | null
+  _base: Component
+}
 
 declare type PropOptions = {
-  type: Function | Array<Function> | null;
-  default: any;
-  required: boolean | null;
-  validator: Function | null;
-};
+  type: Function | Array<Function> | null
+  default: any
+  required: boolean | null
+  validator: Function | null
+}
--- typescript/vnode.d.ts
+++ typescript/vnode.d.ts
@@ -1,41 +1,41 @@
-import VNode from "../src/core/vdom/vnode";
-import { Component } from "./component";
+import VNode from '../src/core/vdom/vnode'
+import { Component } from './component'
 
 declare type VNodeChildren =
   | Array<null | VNode | string | VNodeChildren>
-  | string;
+  | string
 
 declare type VNodeComponentOptions = {
-  Ctor: Component;
-  propsData?: Object;
-  listeners?: Record<string, Function | Function[]>;
-  children?: Array<VNode>;
-  tag?: string;
-};
+  Ctor: Component
+  propsData?: Object
+  listeners?: Record<string, Function | Function[]>
+  children?: Array<VNode>
+  tag?: string
+}
 
 declare type MountedComponentVNode = VNode & {
-  context: Component;
-  componentOptions: VNodeComponentOptions;
-  componentInstance: Component;
-  parent: VNode;
-  data: VNodeData;
-};
+  context: Component
+  componentOptions: VNodeComponentOptions
+  componentInstance: Component
+  parent: VNode
+  data: VNodeData
+}
 
 // interface for vnodes in update modules
 declare type VNodeWithData = VNode & {
-  tag: string;
-  data: VNodeData;
-  children: Array<VNode>;
-  text: void;
-  elm: any;
-  ns: string | void;
-  context: Component;
-  key: string | number | undefined;
-  parent?: VNodeWithData;
-  componentOptions?: VNodeComponentOptions;
-  componentInstance?: Component;
-  isRootInsert: boolean;
-};
+  tag: string
+  data: VNodeData
+  children: Array<VNode>
+  text: void
+  elm: any
+  ns: string | void
+  context: Component
+  key: string | number | undefined
+  parent?: VNodeWithData
+  componentOptions?: VNodeComponentOptions
+  componentInstance?: Component
+  isRootInsert: boolean
+}
 
 // // interface for vnodes in update modules
 // declare type VNodeWithData = {
@@ -54,51 +54,51 @@ declare type VNodeWithData = VNode & {
 // };
 
 declare interface VNodeData {
-  key?: string | number;
-  slot?: string;
-  ref?: string;
-  is?: string;
-  pre?: boolean;
-  tag?: string;
-  staticClass?: string;
-  class?: any;
-  staticStyle?: { [key: string]: any };
-  style?: string | Array<Object> | Object;
-  normalizedStyle?: Object;
-  props?: { [key: string]: any };
-  attrs?: { [key: string]: string };
-  domProps?: { [key: string]: any };
-  hook?: { [key: string]: Function };
-  on?: { [key: string]: Function | Array<Function> };
-  nativeOn?: { [key: string]: Function | Array<Function> };
-  transition?: Object;
-  show?: boolean; // marker for v-show
+  key?: string | number
+  slot?: string
+  ref?: string
+  is?: string
+  pre?: boolean
+  tag?: string
+  staticClass?: string
+  class?: any
+  staticStyle?: { [key: string]: any }
+  style?: string | Array<Object> | Object
+  normalizedStyle?: Object
+  props?: { [key: string]: any }
+  attrs?: { [key: string]: string }
+  domProps?: { [key: string]: any }
+  hook?: { [key: string]: Function }
+  on?: { [key: string]: Function | Array<Function> }
+  nativeOn?: { [key: string]: Function | Array<Function> }
+  transition?: Object
+  show?: boolean // marker for v-show
   inlineTemplate?: {
-    render: Function;
-    staticRenderFns: Array<Function>;
-  };
-  directives?: Array<VNodeDirective>;
-  keepAlive?: boolean;
-  scopedSlots?: { [key: string]: Function };
+    render: Function
+    staticRenderFns: Array<Function>
+  }
+  directives?: Array<VNodeDirective>
+  keepAlive?: boolean
+  scopedSlots?: { [key: string]: Function }
   model?: {
-    value: any;
-    callback: Function;
-  };
+    value: any
+    callback: Function
+  }
 
-  [key: string]: any;
+  [key: string]: any
 }
 
 declare type VNodeDirective = {
-  name: string;
-  rawName: string;
-  value?: any;
-  oldValue?: any;
-  arg?: string;
-  oldArg?: string;
-  modifiers?: ASTModifiers;
-  def?: Object;
-};
+  name: string
+  rawName: string
+  value?: any
+  oldValue?: any
+  arg?: string
+  oldArg?: string
+  modifiers?: ASTModifiers
+  def?: Object
+}
 
 declare type ScopedSlotsData = Array<
   { key: string; fn: Function } | ScopedSlotsData
->;
+>
