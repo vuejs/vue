4cb2b7bd33392933b8d8f767c75add4ef30446b0
Evan You <yyx990803@gmail.com>
2022-06-14 22:17:10 +0800

    wip: expose generateCodeFrame in compiler-sfc

--- packages/compiler-sfc/src/index.ts
+++ packages/compiler-sfc/src/index.ts
@@ -3,6 +3,7 @@ export { parse } from './parse'
 export { compileTemplate } from './compileTemplate'
 export { compileStyle, compileStyleAsync } from './compileStyle'
 export { compileScript } from './compileScript'
+export { generateCodeFrame } from 'compiler/codeframe'
 
 // types
 export { SFCBlock, SFCCustomBlock, SFCDescriptor } from './parseComponent'
