db67e68140e855aecf263cc9bdfa319c7555ffaa
deft <deftmail@163.com>
2022-07-03 03:24:39 +0800

    fix(utils): haschagned fn

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -359,8 +359,8 @@ export function once<T extends (...args: any[]) => any>(fn: T): T {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is#polyfill
 export function hasChanged(x: unknown, y: unknown): boolean {
   if (x === y) {
-    return x === 0 && 1 / x !== 1 / (y as number)
+    return x !== 0 || 1 / x === 1 / (y as number)
   } else {
-    return x === x && y === y
+    return x !== x && y !== y
   }
 }
