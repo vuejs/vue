5cd0143f119672a1f07c4d769c1649c93ea2854a
Yuanyuan Zhao <zhaoyy09@rd.netease.com>
2022-07-28 09:49:07 +0800

    Vue构造函数分析

--- src/core/index.js
+++ src/core/index.js
@@ -1,26 +1,32 @@
-import Vue from './instance/index'
-import { initGlobalAPI } from './global-api/index'
-import { isServerRendering } from 'core/util/env'
-import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
+// 导入Vue
+import Vue from "./instance/index";
+import { initGlobalAPI } from "./global-api/index";
+import { isServerRendering } from "core/util/env";
+import { FunctionalRenderContext } from "core/vdom/create-functional-component";
 
-initGlobalAPI(Vue)
+// 将Vue构造函数作为参数，传递给initGlobalAPI方法，该方法来自 ./global-api/index.js文件
+initGlobalAPI(Vue);
 
-Object.defineProperty(Vue.prototype, '$isServer', {
-  get: isServerRendering
-})
+// 在Vue.prototype上添加$isServer属性，该属性“代理”了来自 core/util/env.js文件的isServerRendering方法
+Object.defineProperty(Vue.prototype, "$isServer", {
+  get: isServerRendering,
+});
 
-Object.defineProperty(Vue.prototype, '$ssrContext', {
-  get () {
+// 在 Vue.prototype 上添加 $ssrContext 属性
+Object.defineProperty(Vue.prototype, "$ssrContext", {
+  get() {
     /* istanbul ignore next */
-    return this.$vnode && this.$vnode.ssrContext
-  }
-})
+    return this.$vnode && this.$vnode.ssrContext;
+  },
+});
 
 // expose FunctionalRenderContext for ssr runtime helper installation
-Object.defineProperty(Vue, 'FunctionalRenderContext', {
-  value: FunctionalRenderContext
-})
+Object.defineProperty(Vue, "FunctionalRenderContext", {
+  value: FunctionalRenderContext,
+});
 
-Vue.version = '__VERSION__'
+// Vue.version存储了当前Vue的版本号
+Vue.version = "__VERSION__";
 
-export default Vue
+// 导出Vue
+export default Vue;
