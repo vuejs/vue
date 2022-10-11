0d6d972b32521fd18eb853b1073c0a19859a499a
Evan You <yyx990803@gmail.com>
2022-10-11 14:58:24 +0800

    fix(compiler-sfc): export parseComponent for compat with fork-ts-checker-webpack-plugin
    fix #12719

--- packages/compiler-sfc/src/index.ts
+++ packages/compiler-sfc/src/index.ts
@@ -6,6 +6,13 @@ export { compileScript } from './compileScript'
 export { generateCodeFrame } from 'compiler/codeframe'
 export { rewriteDefault } from './rewriteDefault'
 
+// For backwards compat only. Some existing tools like
+// fork-ts-checker-webpack-plugin relies on its presence for differentiating
+// between Vue 2 and Vue 3.
+// ref #12719
+// ref https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/765
+export { parseComponent } from './parseComponent'
+
 // types
 export { SFCParseOptions } from './parse'
 export { CompilerOptions, WarningMessage } from 'types/compiler'
