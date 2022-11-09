fc7a228f5cf840fd77781a38b4a35ea1649f690c
Simon He <13917107469@163.com>
2022-11-09 13:19:59 +0800

    refactor: tidy up getFirstComponentChild

--- src/core/vdom/helpers/get-first-component-child.ts
+++ src/core/vdom/helpers/get-first-component-child.ts
@@ -5,12 +5,7 @@ import { isAsyncPlaceholder } from './is-async-placeholder'
 export function getFirstComponentChild(
   children?: Array<VNode>
 ): VNode | undefined {
-  if (isArray(children)) {
-    for (let i = 0; i < children.length; i++) {
-      const c = children[i]
-      if (isDef(c) && (isDef(c.componentOptions) || isAsyncPlaceholder(c))) {
-        return c
-      }
-    }
-  }
+  return isArray(children)
+    ? children.find(c => isDef(c) && (isDef(c.componentOptions) || isAsyncPlaceholder(c)))
+    : undefined
 }
