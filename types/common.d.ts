2e570617666983ababcfd167dfa50208f5dfd45a
Caichong <951412526@qq.com>
2022-12-14 16:43:02 +0800

    chore: comment typos (#12878)
    Co-authored-by: 方恒 <fangheng@shwpg.com>

--- types/common.d.ts
+++ types/common.d.ts
@@ -14,7 +14,7 @@ type Equal<Left, Right> =
 
 export type HasDefined<T> = Equal<T, unknown> extends true ? false : true
 
-// If the the type T accepts type "any", output type Y, otherwise output type N.
+// If the type T accepts type "any", output type Y, otherwise output type N.
 // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
 export type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N
 
