0e8511a8becf627e00443bd799dd99e5fd1b8a35
Vadim <vadimgerasimenko101@gmail.com>
2022-12-28 00:38:05 +0300

    chore: fix typo in apiAsyncComponent.ts (#12912)

--- src/v3/apiAsyncComponent.ts
+++ src/v3/apiAsyncComponent.ts
@@ -47,7 +47,7 @@ export function defineAsyncComponent(
 
   if (__DEV__ && suspensible) {
     warn(
-      `The suspensiblbe option for async components is not supported in Vue2. It is ignored.`
+      `The suspensible option for async components is not supported in Vue2. It is ignored.`
     )
   }
 
