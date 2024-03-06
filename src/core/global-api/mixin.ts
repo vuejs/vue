71634cac8fdfde27b57ff7f5ff5288d255e9d7fa
Jake Mackie <jake.wmackie@gmail.com>
2024-03-06 11:23:01 +0000

    Update mixin.ts (params)
    Added descriptive params to the initMixin function.

--- src/core/global-api/mixin.ts
+++ src/core/global-api/mixin.ts
@@ -1,9 +1,16 @@
 import type { GlobalAPI } from 'types/global-api'
 import { mergeOptions } from '../util/index'
 
+/**
+ * Initializes mixin functionality for Vue.js.
+ *
+ * @param {Object} Vue - The Vue instance with the global API.
+ * @return {Object} - The modified Vue instance with mixin functionality.
+ */
+
 export function initMixin(Vue: GlobalAPI) {
   Vue.mixin = function (mixin: Object) {
-    this.options = mergeOptions(this.options, mixin)
-    return this
-  }
+    this.options = mergeOptions(this.options, mixin);
+    return this;
+  };
 }
